4fa1a2821f31f20be7b6cd7327d9fc0cbb58d953
Eric Jackson <ejackson@ashevillenc.gov>
2020-04-30 13:49:04 -0400

    adding gitignore

--- process_asset_dependencies.py
+++ process_asset_dependencies.py
@@ -1,15 +1,21 @@
 #In progress ... don't beat me up yet.
 import boto3
+import os
 import json
+
+BUCKETNAME = os.getenv('bedrock_bucketname')
+WORKINGDIR = os.getenv('bedrock_workingdir', '.')
+
 s3 = boto3.client('s3')
-objs = s3.list_objects_v2(Bucket='managed-data-assets-dev')['Contents']
+objs = s3.list_objects_v2(Bucket=BUCKETNAME)['Contents']
 dependency_map = {}
 for obj in objs:
     path = obj['Key'].split('/')
     if (len(path) > 1 and (path[-2] + '.json' == path[-1])):
         # This is the configuration file for the asset
-        s3.download_file('managed-data-assets-dev', obj['Key'], './x.json')
-        with open('x.json', 'r') as f:
+        filepath = WORKINGDIR+'/tmp.json'
+        s3.download_file('managed-data-assets-dev', obj['Key'], filepath)
+        with open(filepath, 'r') as f:
             config = json.load(f)
             if (config['active']):
                 dependency_map[config['name']] = {
