a191872b0d43c754057220eb4026adba95bdccc6
Eric Jackson <ejackson@ashevillenc.gov>
2020-04-30 14:44:12 -0400

    Start using toposort

--- process_asset_dependencies.py
+++ process_asset_dependencies.py
@@ -2,12 +2,14 @@
 import boto3
 import os
 import json
+from toposort import toposort
 
 BUCKETNAME = os.getenv('bedrock_bucketname')
 WORKINGDIR = os.getenv('bedrock_workingdir', '.')
 
 s3 = boto3.client('s3')
 objs = s3.list_objects_v2(Bucket=BUCKETNAME)['Contents']
+all_assets = {}
 dependency_map = {}
 for obj in objs:
     path = obj['Key'].split('/')
@@ -18,9 +20,15 @@ for obj in objs:
         with open(filepath, 'r') as f:
             config = json.load(f)
             if (config['active']):
-                dependency_map[config['name']] = {
+                all_assets[config['name']] = {
                     'name': config['name'],
                     'depends': config['depends'],
                     'path': obj['Key']
                 }
+                dependency_map[config['name']] = {i for i in config['depends']}
+print('All Assets')
+print(all_assets)
+print('Dependency Map')
 print(dependency_map)
+print('Sorted Dependencies')
+print(list(toposort(dependency_map)))
