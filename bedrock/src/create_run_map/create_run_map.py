914c2b6aeb7d2e5fbea9afcc680b26f16476e8a2
Eric Jackson <ejackson@ashevillenc.gov>
2020-06-26 13:38:51 -0400

    Initial version of the create-run-map function

--- bedrock/src/create_run_map/create_run_map.py
+++ bedrock/src/create_run_map/create_run_map.py
@@ -3,5 +3,23 @@
 import boto3
 import os
 import json
+from toposort import toposort
 
-# To Do - probably need to write some code here
+def create_run_map_function(bucket_name, run_group):
+    print('Bucket name is ', bucket_name)
+    print('Run group is ', run_group)
+    WORKINGDIR = os.getenv('bedrock_workingdir', '.')
+    s3 = boto3.client('s3')
+    print('Download the asset information')
+    downloaded_file = WORKINGDIR + '/all_assets.json'
+    s3.download_file(bucket_name, 'run/all_assets.json', downloaded_file)
+    print('Now read the downloaded file')
+    with open(downloaded_file, 'r') as file_content:
+        all_assets = json.load(file_content)
+    os.remove(downloaded_file)
+    dependency_map = {}
+    for key, asset in all_assets.items():   # Ought to be a more pythonic approach using filter() and lambda, but punting for now
+        if (asset['run_group'] == run_group):
+            dependency_map[key] = {a for a in asset['depends']}
+
+    return list(toposort(dependency_map))
