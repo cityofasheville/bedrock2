2a25ee5d338bd54a29f5efa2cbee1b9e8b7e64e9
Eric Jackson <ejackson@ashevillenc.gov>
2020-06-30 10:45:10 -0400

    Adding scripts to create bedrock_run_role

---
+++ bedrock/src/aws/bedrock_run_role/create_role.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws iam create-role --role-name bedrock_run_role --assume-role-policy-document file://trust_policy.json
+aws iam attach-role-policy --role-name bedrock_run_role --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+aws iam attach-role-policy --role-name bedrock_run_role --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
