149c6d780d2a863b5baff7dec326dccb1bf2a033
mkornatz <mkornatz@hush.com>
2020-06-30 14:09:53 -0400

    Remove old zip before zipping

--- bedrock/src/build-functions.sh
+++ bedrock/src/build-functions.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 for directory in ./*/
 do
+  echo ""
   echo "Building in $directory..."
-  cd "$directory"
+  cd "$directory" || continue
   echo "Removing old dependencies..."
   rm -Rf ./package
   echo "Installing dependencies..."
   pip install --target ./package
   echo "Packaging function as zip..."
+  rm -f function.zip
   zip function.zip ./*
   cd ..
 done
