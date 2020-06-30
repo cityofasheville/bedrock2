7285cb140603316a5eff06a20c51a65a0ea20c9b
Eric Jackson <ejackson@ashevillenc.gov>
2020-06-30 16:27:10 -0400

    Moving lambda stuff to a separate AWS directory

---
+++ bedrock/aws/lambda_functions/build-functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for directory in ./*/
+do
+  echo ""
+  echo "Building in $directory..."
+  cd "$directory" || continue
+  echo "Removing old dependencies..."
+  rm -Rf ./package
+  echo "Installing dependencies..."
+  #pip install -r requirements.txt --target ./package || ( echo "FAILED TO INSTALL DEPENDENCIES" && continue )
+  echo "Packaging function as zip..."
+  rm -f function.zip
+  #zip function.zip ./*
+  cd ..
+done
