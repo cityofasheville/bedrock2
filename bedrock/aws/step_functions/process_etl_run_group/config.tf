221b4daf4e90223ab7d3e144b78a1f5f8b8cd655
Eric Jackson <ejackson@ashevillenc.gov>
2020-07-10 18:16:33 -0400

    Adding in initial terraform implementation of step function

---
+++ bedrock/aws/step_functions/process_etl_run_group/config.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  profile	= "default"
+  region	= "us-east-1"
+}
+
+resource "aws_iam_role" "process_etl_run_group_role" {
+    name = "process_etl_run_group_role"
+    assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+    {
+    "Effect": "Allow",
+    "Principal": {
+        "Service": "states.amazonaws.com"
+    },
+    "Action": "sts:AssumeRole"
+    }
+]
+}
+EOF
+    tags = {
+        tag-key = "tag-value"
+    }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+    {
+        "Action": [
+            "lambda:InvokeFunction*"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "xray:PutTraceSegments",
+            "xray:PutTelemetryRecords",
+            "xray:GetSamplingRules",
+            "xray:GetSamplingTargets"
+        ],
+        "Resource": [
+            "*"
+        ]
+    }
+]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+    role    = aws_iam_role.process_etl_run_group_role.name
+    policy_arn  = aws_iam_policy.policy.arn
+}
+
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "process_etl_run_group"
+  role_arn = aws_iam_role.process_etl_run_group_role.arn
+
+  definition = file("./hello.json")
+}
