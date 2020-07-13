1d83f8da83a0c1246da27d3d29245b02154070f3
Eric Jackson <ejackson@ashevillenc.gov>
2020-07-13 14:30:19 -0400

    Deleted create-run-map command from controller

--- bedrock/controllers/bedrock_core.py
+++ bedrock/controllers/bedrock_core.py
@@ -2,7 +2,6 @@ from cement import Controller, ex
 import os
 
 from ..src.preprocess_assets.preprocess_assets import preprocess_assets_in_s3
-from ..src.create_run_map.create_run_map import create_run_map_function
 
 class BedrockCore(Controller):
     class Meta:
@@ -25,22 +24,5 @@ class BedrockCore(Controller):
         if self.app.pargs.output is not None:
             output_mode = self.app.pargs.output
         val = preprocess_assets_in_s3(BUCKETNAME, output_mode)
-        print(val)
-        pass
-
-    @ex(
-        help='Create run map',
-
-        arguments=[
-            ### create run map help
-            ( [ '-r', '--rungroup' ],
-              { 'help' : 'run group name)' } ),
-        ],
-    )
-    def create_run_map(self):
-        BUCKETNAME = os.getenv('bedrock_bucketname')
-        if self.app.pargs.rungroup is not None:
-            rungroup = self.app.pargs.rungroup
-        val = create_run_map_function(BUCKETNAME, rungroup)
         print(val)
         pass
