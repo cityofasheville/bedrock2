28258f49d1cb4a5dd43bd972d022f0dcf2ba3092
Eric Jackson <ericjackson@Erics-MacBook-Air.local>
2020-07-14 11:51:26 -0400

    Just say it is done

--- bedrock/aws/lambda_functions/update_run_map/handler.py
+++ bedrock/aws/lambda_functions/update_run_map/handler.py
@@ -7,9 +7,9 @@ import json
 def lambda_handler(event, context):
     print('I am in update_run_map')
     print(json.dumps(event))
+    state = event['state']
+    state['go'] = 'false'
     return {
         'statusCode': 200,
-        'body': {
-            "lambda_output": "Gonna get there some day"
-        }
+        'body': state
     }
