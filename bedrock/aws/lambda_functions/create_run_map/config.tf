af3506ff5737702d1bc11989187d67372c59a469
Eric Jackson <ericjackson@Erics-MacBook-Air.local>
2020-07-17 10:27:34 -0400

    Removed all lambda-specific roles and replaced with bedrock-lambda-role

--- bedrock/aws/lambda_functions/create_run_map/config.tf
+++ bedrock/aws/lambda_functions/create_run_map/config.tf
@@ -11,29 +11,11 @@ provider "aws" {
   region	= "us-east-1"
 }
 
-resource "aws_iam_role" "create_run_map_role" {
-    name = "create_run_map_role"
-    assume_role_policy = file("./role.json")
-    tags = {
-        tag-key = "tag-value"
-    }
-}
-
 resource "aws_lambda_function" "create_run_map" {
     filename        = "function.zip"
     function_name   = "create_run_map"
-    role            = aws_iam_role.create_run_map_role.arn
+    role            = "arn:aws:iam::382274149743:role/bedrock-lambda-role"
     handler         = "handler.lambda_handler"
     runtime         = "python3.8"
     source_code_hash = filebase64sha256("function.zip")
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-    role        = aws_iam_role.create_run_map_role.name
-    policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
-    role        = aws_iam_role.create_run_map_role.name
-    policy_arn  = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
