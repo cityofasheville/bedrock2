0fac0f75299e45590c337883b147587c3d2f050f
Eric Jackson <ericjackson@Erics-MacBook-Air.local>
2020-07-19 14:51:28 -0400

    Adding unknown task type

---
+++ bedrock/aws/lambda_functions/unknown_task/handler.py
@@ -0,0 +1,15 @@
+  
+#!/usr/bin/env python3
+import boto3
+import os
+import json
+
+def lambda_handler(event, context):
+    print(json.dumps(event))
+    task = event['task']
+    return {
+        'statusCode': 404,
+        'body': {
+            "lambda_output": "No such task type " + task['type'] + " defined."
+        }
+    }
