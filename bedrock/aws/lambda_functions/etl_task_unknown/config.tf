d1cb745c0b0dfa41e2b596c305c3d15ddf0cec06
Eric Jackson <ericjackson@Erics-MacBook-Air.local>
2020-07-31 10:05:27 -0400

    Adding new versions of noop and unknown etl tasks

---
+++ bedrock/aws/lambda_functions/etl_task_unknown/config.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    bucket = "cad-tfstate-store"
+    key    = "terraform/bedrock/lambda_functions/etl_task_unknown/terraform_dev.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "region" {
+  type          = string
+  description   = "Region in which to create resources"
+}
+
+variable "lambda_role" {
+  type          = string
+  description   = "Role to use for the lambda function"
+}
+
+provider "aws" {
+  profile	= "default"
+  region	= var.region
+}
+
+resource "aws_lambda_function" "etl_task_unknown" {
+    filename        = "function.zip"
+    function_name   = "etl_task_unknown"
+    role            = var.lambda_role
+    handler         = "handler.lambda_handler"
+    runtime         = "python3.8"
+    source_code_hash = filebase64sha256("function.zip")
+}
