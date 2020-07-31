59b168c780eb242ee0b2b5016cb27ce72bf1c137
Eric Jackson <ericjackson@Erics-MacBook-Air.local>
2020-07-31 10:59:00 -0400

    Adding new versions of create/update_run_map

---
+++ bedrock/aws/lambda_functions/create_etl_run_map/config.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    bucket = "cad-tfstate-store"
+    key    = "terraform/bedrock/lambda_functions/create_etl_run_map/terraform_dev.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "region" {
+  type          = string
+  description   = "Region in which to create resources"
+}
+
+variable "lambda_role" {
+  type          = string
+  description   = "Role to use for the lambda function"
+}
+
+provider "aws" {
+  profile	= "default"
+  region	= var.region
+}
+
+resource "aws_lambda_function" "create_etl_run_map" {
+    filename        = "function.zip"
+    function_name   = "create_etl_run_map"
+    role            = var.lambda_role
+    handler         = "handler.lambda_handler"
+    runtime         = "python3.8"
+    source_code_hash = filebase64sha256("function.zip")
+}
---
+++ bedrock/aws/lambda_functions/update_etl_run_map/handler.py
@@ -0,0 +1,79 @@
+  
+#!/usr/bin/env python3
+import boto3
+import os
+import json
+
+def update_run_map(state):
+    newstate = {}
+    newstate['success'] = state['success']
+    newstate['skipped'] = state['skipped']
+    newstate['failure'] = state['failure']
+
+    jobs = state['next']
+    results = state['results']
+    fails = {}
+    for i in range(len(jobs)):
+        job = jobs[i]
+        result = results[i]['ETLJob']
+        name = result['name']
+        print ('Run ' + str(i) + ' ' + name)
+        for j in range(len(result['etl_tasks'])):
+            task_result = result['etl_tasks'][j]['result']
+            if 'statusCode' not in task_result or task_result['statusCode'] != 200:
+                print('   Task ' + str(j) + ' of job ' + name + ' failed')
+                newstate['failure'].append({
+                    "name": name,
+                    "job": job,
+                    "result": result
+                })
+                fails[name] = True
+                break
+            else:
+                print('   Task ' + str(j) + ' of job ' + name + ' succeeded')
+                newstate['success'].append(name)
+            if name in fails:
+                break
+
+    # Purge all jobs from state['remainder'] that depend on failed or skipped jobs
+    newremainder = []
+    while len(state['remainder']) > 0:
+        jobset = state['remainder'].pop()
+        jobs = []
+        while (len(jobset)) > 0:
+            job = jobset.pop()
+            for i in range(len(job['depends'])):
+                if job['depends'][i] in fails:
+                    fails[job['name']] = True
+                    newstate['skipped'].append(job['name'])
+                    break
+            if job['name'] not in fails:
+                jobs.append(job)
+        if len(jobs) > 0:
+            newremainder.append(jobs)
+
+    # See if there are any more jobs to run
+    newstate['next'] = None
+    newstate['results'] = None
+    if len(newremainder) > 0:
+        newstate['next'] = newremainder.pop()
+        newstate['remainder'] = newremainder
+        newstate['go'] = True
+    else:
+        newstate['go'] = False
+
+    return newstate
+
+
+
+def lambda_handler(event, context):
+    print('I am in update_run_map')
+#    print(json.dumps(event))
+    state = event['state']
+    result = update_run_map(state)
+#    result['go'] = False
+
+    return {
+        'statusCode': 200,
+        'body': result
+    }
