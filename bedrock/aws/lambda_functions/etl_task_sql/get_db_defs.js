09edf75cfcc6dfe3df8fd9b31bd75bb7da6990de
Jon <jtwilson@ashevillenc.gov>
2020-08-10 12:55:37 -0400

    mdaNoDev

--- bedrock/aws/lambda_functions/etl_task_sql/get_db_defs.js
+++ bedrock/aws/lambda_functions/etl_task_sql/get_db_defs.js
@@ -3,7 +3,7 @@ var s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 // Get database auth connection file
 async function get_db_defs() {
-    const params = {Bucket: 'managed-data-assets-dev', Key: 'run/bedrock_connections.json'};
+    const params = {Bucket: 'managed-data-assets', Key: 'run/bedrock_connections.json'};
     let data = await s3.getObject(params).promise();
     let db_defs = JSON.parse(data.Body.toString('utf8')); 
 
