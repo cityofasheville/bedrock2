487f7f190fdc806c83d86b5718b912ff900f5b03
Jon <jtwilson@ashevillenc.gov>
2020-12-30 13:09:37 -0500

    email improvements

--- bedrock/aws/lambda_functions/etl_email_results/sendEmails.js
+++ bedrock/aws/lambda_functions/etl_email_results/sendEmails.js
@@ -8,12 +8,17 @@ function sendEmails(results) {
     results.failure = results.failure.map(res=>res.name) 
     results.failure.sort() 
     results.skipped.sort()
-    
+
+    let emailSubject = "ETL Jobs Status: OK"
+    if (results.skipped.length > 0 || results.failure.length > 0) {
+        emailSubject = "ETL Jobs Status: Error"
+    }
+
     let emailAddrs = ["jtwilson@ashevillenc.gov"]
     let pugObj = {};
     pugObj.results = results;
     let htmlEmail = compiledFunction(pugObj);
-    ses_sendemail(emailAddrs,htmlEmail);
+    ses_sendemail(emailAddrs,htmlEmail,emailSubject);
 }
 
 module.exports = sendEmails;
--- bedrock/aws/lambda_functions/etl_email_results/ses_sendemail.js
+++ bedrock/aws/lambda_functions/etl_email_results/ses_sendemail.js
@@ -27,9 +27,10 @@ let params = {
   ],
 };
 
-function ses_sendemail(emailAddrs, htmlEmail){
+function ses_sendemail(emailAddrs, htmlEmail, emailSubject){
     params.Destination.ToAddresses = emailAddrs;
     params.Message.Body.Html.Data = htmlEmail;
+    params.Message.Subject.Data = emailSubject
 
     // Create the promise and SES service object
     let sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
