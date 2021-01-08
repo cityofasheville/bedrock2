11335de8064c46509bcf5dd79eb1f136cbfa7c12
Eric Jackson <ejackson@ashevillenc.gov>
2021-01-08 17:30:23 -0500

    Cleaner implementation of DB types to bedrock types

--- bedrock/src/utilities/types.py
+++ bedrock/src/utilities/types.py
@@ -14,148 +14,130 @@ bedrock_types = [
 
 ss_types = {
     "character": {
-        "bedrock_type": "character",
-        "default": True
+        "bedrock_type": "character"
     },
     "character varying": {
-        "bedrock_type": "string",
-        "default": False
+        "bedrock_type": "string"
     },
     "varchar": {
-        "bedrock_type": "string",
-        "default": True
-    },
-    "integer": {
-        "bedrock_type": "integer",
-        "length": 4,
-        "default": True
+        "bedrock_type": "string"
     },
     "int": {
         "bedrock_type": "integer",
-        "length": 4,
-        "default": False
+        "length": 4
     },
     "bigint": {
         "bedrock_type": "integer",
-        "length": 8,
-        "default": False
+        "length": 8
     },
     "smallint": {
         "bedrock_type": "integer",
-        "length": 2,
-        "default": False
+        "length": 2
+    },
+    "tinyint": {
+        "bedrock_type": "integer",
+        "length": 1
     },
     "bit": {
-        "bedrock_type": "boolean",
-        "default": True
+        "bedrock_type": "boolean"
     },
     "real": {
         "bedrock_type": "real",
-        "length": 4,
-        "default": True
+        "length": 4
     },
-    "double precision": {
-        "bedrock_type": "real",
-        "length": 8,
-        "default": False
+    "float": {
+        "bedrock_type": "real"
     },
     "numeric": {
-        "bedrock_type": "decimal",
-        "default": True
+        "bedrock_type": "decimal"
+    },
+    "decimal": {
+        "bedrock_type": "decimal"
     },
     "datetime": {
-        "bedrock_type": "datetime",
-        "default": True
+        "bedrock_type": "datetime"
     },
     "date": {
-        "bedrock_type": "date",
-        "default": True
+        "bedrock_type": "date"
     },
     "time": {
-        "bedrock_type": "time",
-        "default": true
+        "bedrock_type": "time"
     },
     "binary": {
-        "bedrock_type": "binary",
-        "default": true
+        "bedrock_type": "binary"
     },
     "varbinary": {
         "bedrock_type": "binary",
-        "default": true
     }
 }
 
 pg_types = {
     "character": {
-        "bedrock_type": "character",
-        "default": True
+        "bedrock_type": "character"
     },
     "text": {
-        "bedrock_type": "string",
-        "default": False
+        "bedrock_type": "string"
     },
     "varchar": {
         "bedrock_type": "string",
-        "alt": "text",
-        "default": True
+        "alt": "text"
+    },
+    "character varying": {
+        "bedrock_type": "string",
+        "alt": "text"
     },
     "integer": {
         "bedrock_type": "integer",
-        "length": 4,
-        "default": True
+        "length": 4
     },
     "int": {
         "bedrock_type": "integer",
-        "length": 4,
-        "default": False
+        "length": 4
     },
     "bigint": {
         "bedrock_type": "integer",
-        "length": 8,
-        "default": False
+        "length": 8
     },
     "smallint": {
         "bedrock_type": "integer",
-        "length": 2,
-        "default": False
+        "length": 2
     },
-    "bit": {
-        "bedrock_type": "boolean",
-        "default": True
+    "boolean": {
+        "bedrock_type": "boolean"
     },
     "real": {
         "bedrock_type": "real",
-        "length": 4,
-        "default": True
+        "length": 4
     },
     "double precision": {
         "bedrock_type": "real",
-        "length": 8,
-        "default": False
+        "length": 8
     },
     "numeric": {
-        "bedrock_type": "decimal",
-        "default": True
+        "bedrock_type": "decimal"
     },
-    "datetime": {
-        "bedrock_type": "datetime",
-        "default": True
+    "decimal": {
+        "bedrock_type": "decimal"
+    },
+    "timestamp without time zone": {
+        "bedrock_type": "datetime"
     },
     "date": {
-        "bedrock_type": "date",
-        "default": True
+        "bedrock_type": "date"
     },
     "time": {
-        "bedrock_type": "time",
-        "default": true
+        "bedrock_type": "time"
     },
-    "binary": {
-        "bedrock_type": "binary",
-        "default": true
+    "time without time zone": {
+        "bedrock_type": "time"
     },
-    "varbinary": {
-        "bedrock_type": "binary",
-        "default": true
+    "bytea": {
+        "bedrock_type": "binary"
+    },
+    "USER-DEFINED": {
+        "bedrock_type": "UNKNOWN"
     }
 }
+
+
 
