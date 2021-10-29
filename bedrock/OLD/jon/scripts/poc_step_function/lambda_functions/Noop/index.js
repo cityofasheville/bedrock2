c9566f172f611fba55593e3418353c63f187999c
Eric Jackson <ejackson@ashevillenc.gov>
2021-10-29 16:36:39 -0400

    starting to remove main code

--- bedrock/OLD/jon/scripts/poc_step_function/lambda_functions/Noop/index.js
+++
@@ -1,5 +0,0 @@
-exports.handler = async function(event, context) {
-  console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2));
-  console.log("EVENT\n" + JSON.stringify(event, null, 2));
-  return context.logStreamName;
-}
