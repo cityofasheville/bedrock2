f6dbe59bde362b20edbab537bcc97f5c0315f18e
Eric Jackson <ejackson@ashevillenc.gov>
2022-08-30 10:16:43 -0400

    Added in use of variables in infrastructure defs for roles

--- src/roles/bedrock-lambda-role/config.tf
+++ src/roles/bedrock-lambda-role/config.tf
@@ -4,10 +4,10 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "bedrock-lambda-role" {
-    name = "bedrock-lambda-role"
+    name = "bedrock-lambda-role-${var.branch}"
     assume_role_policy = file("./policy_role.json")
     tags = {
-        Name          = "bedrock-lambda-role"
+        Name          = "bedrock-lambda-role-${var.branch}"
         Application   = "bedrock"
         Environment   = "development"
         TechnicalLead = "custom-dev"
@@ -19,44 +19,44 @@ resource "aws_iam_role" "bedrock-lambda-role" {
 }
 
 resource "aws_iam_policy" "secrets_manager_policy" {
-  name        = "secrets_manager_policy"
+  name        = "secrets_manager_policy-${var.branch}"
   description = "Read secrets"
   policy = templatefile("./policy_secrets_manager.json",{})
 }
 
 resource "aws_iam_policy" "invoke_lambda_policy" {
-  name        = "invoke_lambda_policy"
+  name        = "invoke_lambda_policy-${var.branch}"
   description = "Invoke another Lambda"
   policy = templatefile("./policy_invoke_lambda.json",{})
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
-    role        = aws_iam_role.bedrock-lambda-role.name
+    role        = format("aws_iam_role.bedrock-lambda-role-%s.name", var.branch)
     policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
-    role        = aws_iam_role.bedrock-lambda-role.name
+    role        = format("aws_iam_role.bedrock-lambda-role-%s.name", var.branch)
     policy_arn  = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_ses_access" {
-    role        = aws_iam_role.bedrock-lambda-role.name
+    role        = format("aws_iam_role.bedrock-lambda-role-%s.name", var.branch)
     policy_arn  = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
-    role        = aws_iam_role.bedrock-lambda-role.name
+    role        = format("aws_iam_role.bedrock-lambda-role-%s.name", var.branch)
     policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "secrets_manager" {
-    role        = aws_iam_role.bedrock-lambda-role.name
+    role        = format("aws_iam_role.bedrock-lambda-role-%s.name", var.branch)
     policy_arn  = aws_iam_policy.secrets_manager_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "invoke_lambda_policy" {
-    role        = aws_iam_role.bedrock-lambda-role.name
+    role        = format("aws_iam_role.bedrock-lambda-role-%s.name", var.branch)
     policy_arn  = aws_iam_policy.invoke_lambda_policy.arn
 }
 
--- src/roles/bedrock-lambda-role/variables.tf
+++ src/roles/bedrock-lambda-role/variables.tf
@@ -8,3 +8,8 @@ variable "aws_profile" {
   type          = string
   description   = "AWS User Profile to use"
 }
+
+variable "branch" {
+  type          = string
+  description   = "Github environment or branch"
+}
