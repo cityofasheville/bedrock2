b1e7eb6261787eb4de918c65ff4f536ab8dce146
cameronhenshaw <chenshaw@ashevillenc.gov>
2022-09-09 09:00:24 -0400

    Deleted optional time parameter, and make the minutes into a constant that you can edit in one place in the script

--- aws/lambda_functions/create_etl_run_map/handler.py
+++ aws/lambda_functions/create_etl_run_map/handler.py
@@ -7,14 +7,16 @@ from datetime import timedelta
 import datetime
 from pyawscron import AWSCron
 from zoneinfo import ZoneInfo
-import math
 
 utc = ZoneInfo('UTC')
 localtz = ZoneInfo('America/New_York')
 dynamodb = boto3.resource('dynamodb')
 
+# This parameter sets the time interval that this code with cover.
+# !!! MAKE SURE THIS MATCHES EVENTBRIDGE RULE
+TIME_INTERVAL = 15
 
-def create_run_map_function(bucket_name, run_group=None, time=None):
+def create_run_map_function(bucket_name, run_group=None):
     WORKINGDIR = '.'
     s3 = boto3.client('s3', region_name='us-east-1')
     downloaded_file = WORKINGDIR + '/all_assets.json'
@@ -24,22 +26,13 @@ def create_run_map_function(bucket_name, run_group=None, time=None):
     os.remove(downloaded_file)
     dependency_map = {}
 
-    if run_group==None: #if no rungroup specified, function checks time and compares with cron expressions
-        if time==None: #if no time specified, function takes current time
-            my_date = rounder(datetime.datetime.now())
-        else:
-            my_date = process_date(time)
-            my_date = rounder(my_date)
-        data = get_table()
-        final_list = get_rungroup_matches(data, my_date)
-        for key, asset in all_assets.items():  # Ought to be a more pythonic approach using filter() and lambda, but punting for now
-            if (asset['run_group'] in final_list): #This code repeats below, but I'm not making it a function since we'll eventually be deleting that when we move to calling this lambda every 15 min
-                dependency_map[key] = {a for a in asset['depends']}
-    
-    else:
-        for key, asset in all_assets.items():   # Ought to be a more pythonic approach using filter() and lambda, but punting for now
-            if (asset['run_group'] == run_group):
-                dependency_map[key] = {a for a in asset['depends']}
+    my_date = process_date()
+    my_date = rounder(my_date)
+    data = get_table()
+    final_list = get_rungroup_matches(data, my_date)
+    for key, asset in all_assets.items():  # Ought to be a more pythonic approach using filter() and lambda, but punting for now
+        if (asset['run_group'] in final_list): 
+            dependency_map[key] = {a for a in asset['depends']}
 
     runsets= list(toposort(dependency_map))
     runs = []
@@ -64,18 +57,18 @@ def create_run_map_function(bucket_name, run_group=None, time=None):
     return result    
 
 
-def process_date(date):
+def process_date():
     #create datetime object, convert to utc
-    dt = datetime.datetime.strptime(date, "%d/%m/%y %H:%M")
+    dt = datetime.datetime.now()
     dt.replace(tzinfo=localtz)
     utctime = dt.astimezone(utc)
     return(utctime)
 
 
 def rounder(t):
-    # Rounds to nearest 15 minutes
+    # Rounds to nearest N minutes
     return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
-               +timedelta(minutes=(math.floor(t.minute/15)*15)))
+               +timedelta(minutes=(round(t.minute/TIME_INTERVAL)*TIME_INTERVAL)))
 
 
 def get_table():
@@ -91,13 +84,13 @@ def get_table():
 
 
 def get_rungroup_matches(data, date):
-    #creates 15 min time range, goes through the crons and see if they fall within in
+    #creates N min time range, goes through the crons and see if they fall within in
     matchList = []
     for entry in data:
         cron = entry['cron']
         # the timezone is already utc, but for some reason it's not working unless I replace it below
         start_date = date.replace(tzinfo=datetime.timezone.utc)
-        end_date = start_date + timedelta(minutes=14)
+        end_date = start_date + timedelta(minutes=(TIME_INTERVAL-1), seconds = 59)
         matches = AWSCron.get_all_schedule_bw_dates(start_date, end_date, cron)
         if matches != []:
             matchList.append(entry['key'])
@@ -117,4 +110,3 @@ def lambda_handler(event, context):
         'body': json.loads(json.dumps(result, default=convert_set_to_list))
     }
 #-------------------------------------------------
-
