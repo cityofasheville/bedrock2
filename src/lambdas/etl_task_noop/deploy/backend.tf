8ddad1bb1e55f491801791a25a251962f3b31dd8
Eric Jackson <ejackson@ashevillenc.gov>
2022-11-23 12:49:02 -0500

    Converted etl_task_noop lambda to use INSTANCE instead of BRANCH, plus fixed dependency on changes in deploy directory

--- src/lambdas/etl_task_noop/deploy/backend.tf
+++ src/lambdas/etl_task_noop/deploy/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     region = $$region$$
     bucket = $$statebucket$$
-    key = "terraform/bedrock/$$BRANCH$$/lambdas/etl_task_noop/terraform_dev.tfstat"
+    key = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_noop/terraform_dev.tfstat"
   }
 }
