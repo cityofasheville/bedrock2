680748c4e7ecada46064e979bad7d873b0ee6a61
Eric Jackson <ejackson@ashevillenc.gov>
2022-11-24 10:12:18 -0500

    Added etl_task_unknown lambda and fixed the backend for update_etl_run_map

---
+++ src/lambdas/update_etl_run_map/deploy/backend.tf
@@ -0,0 +1,7 @@
+terraform { 
+  backend "s3" {
+    region = $$region$$
+    bucket = $$statebucket$$
+    key = "terraform/bedrock/$$INSTANCE$$/lambdas/update_etl_run_map/terraform_dev.tfstat"
+  }
+}
