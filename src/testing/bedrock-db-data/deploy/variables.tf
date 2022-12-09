8484cfa57eabc88d2c13067e6c8acc094fbeecda
Jon Wilson <jontwilson@gmail.com>
2022-12-09 12:49:02 -0500

    wip adding bedrock-db-data

---
+++ src/testing/bedrock-db-data/deploy/variables.tf
@@ -0,0 +1,9 @@
+
+variable "region" {
+  type          = string
+  description   = "Region in which to create resources"
+}
+variable "instance" {
+  type          = string
+  description   = "Github environment or branch"
+}
