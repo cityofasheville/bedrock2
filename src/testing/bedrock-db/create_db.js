77c7c3245b1b7854632ca7b78fb9b53e610f60ce
Eric Jackson <ejackson@ashevillenc.gov>
2022-12-10 18:19:43 -0500

    Code to create RDS instance and create database tables

---
+++ src/testing/bedrock-db/create_db.js
@@ -0,0 +1,24 @@
+const { Client } = require('pg');
+const fs = require('fs');
+
+lambda_handler = async function (event, context) {
+  let sql = fs.readFileSync('./createNewBedrockDB.sql').toString();
+  const host = process.env.DB_ENDPOINT.split(':')[0];
+  console.log('The host is ', host);
+  const client = new Client({
+    host: host,
+    user: 'bedrock',
+    password: 'test-bedrock',
+    database: 'bedrock',
+    max: 10,
+    idleTimeoutMillis: 10000,
+  });
+  await client.connect()
+  const res = await client.query(sql);
+
+  await client.end()
+  return res;
+}
+
+lambda_handler()
+
--- src/testing/bedrock-db/deploy/config.tf
+++ src/testing/bedrock-db/deploy/config.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_db_instance" "bedrock-db-$$INSTANCE$$" {
   allocated_storage    = 10
-  db_name              = "bedrock_db_$$INSTANCE$$"
+  db_name              = "bedrock"
   identifier           = "bedrock-db-$$INSTANCE$$"
   engine               = "postgres"
   engine_version       = "13.7"
@@ -13,5 +13,38 @@ resource "aws_db_instance" "bedrock-db-$$INSTANCE$$" {
   password             = "test-bedrock"
   parameter_group_name = "default.postgres13"
   skip_final_snapshot  = true
-  db_subnet_group_name = "bpt-db-subnet-group"
+  publicly_accessible  = true
+  db_subnet_group_name = "public-db-subnet-group"
+  vpc_security_group_ids = [aws_security_group.bedrock-pg-sg-$$INSTANCE$$.id]
+}
+
+resource "aws_security_group" "bedrock-pg-sg-$$INSTANCE$$" {
+  name        = "bedrock-pg-sg-$$INSTANCE$$"
+  description = "Allow public access to postgres"
+  vpc_id      = "vpc-0340a3823e5b5f0d2"
+
+  ingress {
+    description      = "Inbound access to postgres"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "bedrock-pg-sg-$$INSTANCE$$"
+  }
+}
+
+output "DB_ENDPOINT" {
+  value = "${aws_db_instance.bedrock-db-$$INSTANCE$$.endpoint}"
 }
