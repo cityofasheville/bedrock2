6df8b22d004d8f2ee52e01b32da512648819598a
Jon Wilson <jontwilson@gmail.com>
2022-12-12 14:06:34 -0500

    added other database table loads

--- src/testing/bedrock-db-data/load_assets.py
+++ src/testing/bedrock-db-data/load_assets.py
@@ -25,27 +25,84 @@ cur = conn.cursor()
 
 print("Database host is ", db_host)
 
+# clear out old data
+sql = f'''
+  truncate table bedrock.assets;
+  truncate table bedrock.dependencies;
+  truncate table bedrock.etl;
+  truncate table bedrock.tasks;
+'''
+cur.execute(sql)
+
 assets_directory = './assets'
-for item in os.listdir(assets_directory):
-  print('Processing ', item)
-  d = os.path.join(assets_directory, item)
+for asset_name in os.listdir(assets_directory):
+  # each asset
+  print('Processing ', asset_name)
+  d = os.path.join(assets_directory, asset_name)
   if os.path.isdir(d):
-    configFile = None
-    etlFile = None
     for file in os.listdir(d):
-      if file == item + '.json':
-        with open(os.path.join(d,file), 'r') as ff:
+      if file == asset_name + '.json':
+        # configFile
+        with open(os.path.join(d, file), 'r') as ff:
           config = json.load(ff)
-      elif file == item + '.etl.json':
-        etl = None
-    sql = f'''
-      insert into bedrock.assets
-      (asset_name, description, location, active)
-      values('{config["name"]}', '{config["description"]}', '{config["location"]}', {"true" if config["active"] else "false"})
-      returning asset_name;
-    '''
-    cur.execute(sql)
-    nm = cur.fetchone()[0] # probably can skip this, but maybe to double check
+          sql = f'''
+            insert into bedrock.assets
+            (asset_name, description, location, active)
+            values(%s, %s, %s, %s);
+          '''
+          cur.execute(sql,
+          (asset_name, config["description"], config["location"], config["active"]
+          ))
+          dependencies = config['depends']
+          
+          if dependencies:
+            for depend in dependencies:
+              sql = f'''
+              insert into bedrock.dependencies(asset_name, dependency)
+              values(%s, %s);
+              '''
+              cur.execute(sql, (asset_name, depend))
+      elif file == asset_name + '.etl.json':
+        # etlFile
+        with open(os.path.join(d, file), 'r') as ff:
+          etl = json.load(ff)
+          sql = f'''
+          insert into bedrock.etl(asset_name, run_group, active)
+          values(%s, %s, %s);
+          '''
+          cur.execute(sql,(asset_name, etl['run_group'], "true"))
+          tasks = etl['tasks']
+          for seq_number, task in enumerate(tasks):
+            type = task['type']
+            if type == "table_copy" or type == "file_copy":
+              # table_copy / file_copy
+              sql = f'''
+              insert into bedrock.tasks(asset_name, seq_number, type, active, source, target, configuration)
+              values(%s, %s, %s, %s, %s, %s, %s);
+              '''
+              cur.execute(sql, (asset_name, seq_number, type, task['active'], 
+              json.dumps(task['source_location']), json.dumps(task['target_location']), None))
+            elif type == "sql":
+              # sql
+              sql_filename = task['file']
+              with open(os.path.join(d, sql_filename), 'r') as sqlfile:
+                sqlstring = sqlfile.read()
+              sql = f'''
+              insert into bedrock.tasks(asset_name, seq_number, type, active, source, target, configuration)
+              values(%s, %s, %s, %s, %s, %s, %s);
+              '''
+              cur.execute(sql, (asset_name, seq_number, type, task['active'], 
+              None, json.dumps({ "connection": task['connection'] }), sqlstring))
+            else:
+              # everything else
+              sql = f'''
+              insert into bedrock.tasks(asset_name, seq_number, type, active, source, target, configuration)
+              values(%s, %s, %s, %s, %s, %s, %s);
+              '''
+              cur.execute(sql, (asset_name, seq_number, type, task['active'], 
+              None, json.dumps(task), None))
+
+    # nm = cur.fetchone()[0] # probably can skip this, but maybe to double check
 
 
 conn.commit()
