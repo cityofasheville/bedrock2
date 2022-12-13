a9433c59eb5118b1b20a58153899f53a7a507f58
Eric Jackson <ejackson@ashevillenc.gov>
2022-12-13 17:09:57 -0500

    Added apply-y target to roles. Removed aws-profile variable from lambda role

--- src/roles/bedrock-lambda-role/deploy/variables.tf
+++ src/roles/bedrock-lambda-role/deploy/variables.tf
@@ -4,11 +4,6 @@ variable "region" {
   description   = "Region in which to create resources"
 }
 
-variable "aws_profile" {
-  type          = string
-  description   = "AWS User Profile to use"
-}
-
 variable "instance" {
   type          = string
   description   = "Name of the current instance"
