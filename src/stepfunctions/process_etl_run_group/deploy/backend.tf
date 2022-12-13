7d94746bff294d3311cbccb32dd5b52055b5a0b3
Eric Jackson <ejackson@ashevillenc.gov>
2022-12-13 18:29:40 -0500

    Fixed all the key names and references for the s3 backend

--- src/stepfunctions/process_etl_run_group/deploy/backend.tf
+++ src/stepfunctions/process_etl_run_group/deploy/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     region = $$region$$
     bucket = $$statebucket$$
-    key = "terraform/bedrock/$$INSTANCE$$/stepfunctions/process_etl_run_group/terraform_dev.tfstat"
+    key = "terraform/bedrock/$$INSTANCE$$/stepfunctions/process_etl_run_group/terraform.tfstate"
   }
 }
--- src/stepfunctions/process_etl_run_group/deploy/datablocks.tf
+++ src/stepfunctions/process_etl_run_group/deploy/datablocks.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "stepfunction_role" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/roles/bedrock-stepfunction-role/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/roles/bedrock-stepfunction-role/terraform.tfstate"
     region = var.region
   }
 }
@@ -12,7 +12,7 @@ data "terraform_remote_state" "create_etl_run_map_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/create_etl_run_map/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/create_etl_run_map/terraform.tfstate"
     region = var.region
   }
 }
@@ -21,7 +21,7 @@ data "terraform_remote_state" "update_etl_run_map_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/update_etl_run_map/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/update_etl_run_map/terraform.tfstate"
     region = var.region
   }
 }
@@ -30,7 +30,7 @@ data "terraform_remote_state" "setup_etl_job_task_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/setup_etl_job_task/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/setup_etl_job_task/terraform.tfstate"
     region = var.region
   }
 }
@@ -39,7 +39,7 @@ data "terraform_remote_state" "check_etl_job_task_status_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/check_etl_job_task_status/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/check_etl_job_task_status/terraform.tfstate"
     region = var.region
   }
 }
@@ -48,7 +48,7 @@ data "terraform_remote_state" "etl_task_noop_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_noop/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_noop/terraform.tfstate"
     region = var.region
   }
 }
@@ -57,7 +57,7 @@ data "terraform_remote_state" "etl_task_sql_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_sql/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_sql/terraform.tfstate"
     region = var.region
   }
 }
@@ -66,7 +66,7 @@ data "terraform_remote_state" "etl_task_table_copy_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_table_copy/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_table_copy/terraform.tfstate"
     region = var.region
   }
 }
@@ -75,7 +75,7 @@ data "terraform_remote_state" "etl_task_sftp_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_sftp/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_sftp/terraform.tfstate"
     region = var.region
   }
 }
@@ -84,7 +84,7 @@ data "terraform_remote_state" "etl_task_encrypt_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_encrypt/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_encrypt/terraform.tfstate"
     region = var.region
   }
 }
@@ -93,7 +93,7 @@ data "terraform_remote_state" "etl_task_file_copy_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_file_copy/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_file_copy/terraform.tfstate"
     region = var.region
   }
 }
@@ -102,7 +102,7 @@ data "terraform_remote_state" "etl_task_unknown_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_unknown/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_task_unknown/terraform.tfstate"
     region = var.region
   }
 }
@@ -111,7 +111,7 @@ data "terraform_remote_state" "etl_email_results_lambda" {
   backend = "s3"
   config = {
     bucket = var.statebucket
-    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_email_results/terraform_dev.tfstat"
+    key    = "terraform/bedrock/$$INSTANCE$$/lambdas/etl_email_results/terraform.tfstate"
     region = var.region
   }
 }
