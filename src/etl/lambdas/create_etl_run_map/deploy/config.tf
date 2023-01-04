2bd7a3b3f463d204c62345ec0d986f6ac9ddb200
Jon Wilson <jontwilson@gmail.com>
2023-01-04 09:00:31 -0500

    add vpc to create_etl_run_map

--- src/etl/lambdas/create_etl_run_map/deploy/config.tf
+++ src/etl/lambdas/create_etl_run_map/deploy/config.tf
@@ -9,6 +9,11 @@ resource "aws_lambda_function" "create_etl_run_map-$$INSTANCE$$" {
     handler         = "handler.lambda_handler"
     runtime         = "nodejs14.x"
     source_code_hash = filebase64sha256("function.zip")
+    timeout         = 900
+    vpc_config {
+      subnet_ids         = var.subnet_ids
+      security_group_ids = var.security_group_ids
+    }
 }
 
 output "create_etl_run_map_arn" {
--- src/etl/lambdas/create_etl_run_map/deploy/variables.tf
+++ src/etl/lambdas/create_etl_run_map/deploy/variables.tf
@@ -3,6 +3,16 @@ variable "region" {
   description   = "Region in which to create resources"
 }
 
+variable "subnet_ids" {
+  type          = list(string)
+  description   = "Array of subnet ids"
+}
+
+variable "security_group_ids" {
+  type          = list(string)
+  description   = "Array of security_group_ids"
+}
+
 variable "rolekey" {
   type          = string
   description   = "Lambda role to assume"
