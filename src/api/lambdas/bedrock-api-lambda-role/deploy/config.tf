1681879e6accdc253bc6c2496e79b1169f924b11
Eric Jackson <ejackson@ashevillenc.gov>
2023-02-23 15:07:27 -0500

    Created the API lambda role

--- src/api/lambdas/bedrock-api-lambda-role/deploy/config.tf
+++ src/api/lambdas/bedrock-api-lambda-role/deploy/config.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region	= var.region
 }
 
-resource "aws_iam_role" "bedrock-lambda-role-$$INSTANCE$$" {
-    name = "bedrock-lambda-role-${var.instance}"
+resource "aws_iam_role" "bedrock-api-lambda-role-$$INSTANCE$$" {
+    name = "bedrock-api-lambda-role-${var.instance}"
     assume_role_policy = file("./policy_role.json")
     tags = {
-      Name          = "bedrock-lambda-role-${var.instance}"
+      Name          = "bedrock-api-lambda-role-${var.instance}"
       "coa:application" = "bedrock"
       "coa:department"  = "information-technology"
       "coa:owner"       = "jtwilson@ashevillenc.gov"
@@ -28,35 +28,35 @@ resource "aws_iam_policy" "invoke_lambda_policy-$$INSTANCE$$" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic-$$INSTANCE$$" {
-    role        = aws_iam_role.bedrock-lambda-role-$$INSTANCE$$.name
+    role        = aws_iam_role.bedrock-api-lambda-role-$$INSTANCE$$.name
     policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_access-$$INSTANCE$$" {
-    role        = aws_iam_role.bedrock-lambda-role-$$INSTANCE$$.name
+    role        = aws_iam_role.bedrock-api-lambda-role-$$INSTANCE$$.name
     policy_arn  = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_ses_access-$$INSTANCE$$" {
-    role        = aws_iam_role.bedrock-lambda-role-$$INSTANCE$$.name
+    role        = aws_iam_role.bedrock-api-lambda-role-$$INSTANCE$$.name
     policy_arn  = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_vpc_access-$$INSTANCE$$" {
-    role        = aws_iam_role.bedrock-lambda-role-$$INSTANCE$$.name
+    role        = aws_iam_role.bedrock-api-lambda-role-$$INSTANCE$$.name
     policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "secrets_manager-$$INSTANCE$$" {
-    role        = aws_iam_role.bedrock-lambda-role-$$INSTANCE$$.name
+    role        = aws_iam_role.bedrock-api-lambda-role-$$INSTANCE$$.name
     policy_arn  = aws_iam_policy.secrets_manager_policy-$$INSTANCE$$.arn
 }
 
 resource "aws_iam_role_policy_attachment" "invoke_lambda_policy-$$INSTANCE$$" {
-    role        = aws_iam_role.bedrock-lambda-role-$$INSTANCE$$.name
+    role        = aws_iam_role.bedrock-api-lambda-role-$$INSTANCE$$.name
     policy_arn  = aws_iam_policy.invoke_lambda_policy-$$INSTANCE$$.arn
 }
 
 output "bedrock_lambda_role_arn" {
-  value = "${aws_iam_role.bedrock-lambda-role-$$INSTANCE$$.arn}"
+  value = "${aws_iam_role.bedrock-api-lambda-role-$$INSTANCE$$.arn}"
 }
--- src/api/lambdas/bedrock-lambda-role/deploy/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    region = $$region$$
-    bucket = $$statebucket$$
-    key = "terraform/bedrock/$$INSTANCE$$/roles/bedrock-lambda-role/terraform.tfstate"
-  }
-}
