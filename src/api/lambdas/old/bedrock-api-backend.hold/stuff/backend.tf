964194689781e7dab275ccb1a44603d9bedc39d5
Eric Jackson <ejackson@ashevillenc.gov>
2023-02-27 18:30:04 -0500

    Adding the higher-level makefiles for the API

--- src/api/lambdas/old/bedrock-api-backend.hold/stuff/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform { 
-  backend "s3" {
-    region = "us-east-1"
-    bucket = "avl-tfstate-store"
-    key = "terraform/bedrock/ejdev/api/lambdas/bedrock_api_backend/terraform.tfstate"
-  }
-}
