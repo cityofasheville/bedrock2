25d5ad7cb639aebbb0f2ea50994e98fbd31c080a
Eric Jackson <ejackson@ashevillenc.gov>
2023-03-02 15:32:34 -0500

    Implemented full skeleton of API for assets

--- src/api/lambdas/bedrock-api-backend/handleAssets.js
+++ src/api/lambdas/bedrock-api-backend/handleAssets.js
@@ -17,32 +17,111 @@ function getAsset (pathElements, queryParams, connection) {
 
 // eslint-disable-next-line no-unused-vars
 function handleAssets(event, pathElements, queryParams, verb, connection) {
-  let ok = true;
-  let message = '';
-  let result;
+  const result = {
+    error: false,
+    message: '',
+    result: null,
+  };
 
   switch (pathElements.length) {
+    // GET assets
     case 1:
-      ok = false;
-      message = 'Not yet implemented: GET /bedrock/assets [?rungroups=...][?/& period=...]';
-      console.log(message);
+      result.message = 'Get all assets not yet implemented';
+      result.error = true;
       break;
 
-    case 2: // Single asset
-      if (verb === 'GET') {
-        return getAsset(pathElements, queryParams, connection);
+    // VERB assets/{assetname}
+    case 2:
+      switch (verb) {
+        case 'GET':
+          result.result = getAsset(pathElements, queryParams, connection);
+          break;
+
+        case 'POST':
+          result.message = 'Add asset not implemented';
+          result.error = true;
+          break;
+
+        case 'PUT':
+          result.message = 'Update asset not implemented';
+          result.error = true;
+          break;
+
+        case 'DELETE':
+          result.message = 'Delete asset not implemented';
+          result.error = true;
+          break;
+
+        default:
+          result.message = `handleAssets: unknown verb ${verb}`;
+          result.error = true;
+          break;
+      }
+      break;
+
+    // GET/DELETE assets/{assetname}/tasks OR
+    // GET assets/{assetname}/depends
+    case 3:
+      if (pathElements[2] === 'tasks') {
+        if (verb === 'GET') {
+          result.message = 'Get asset tasks not implemented';
+          result.error = true;
+        } else if (verb === 'DELETE') {
+          result.message = 'Delete all asset tasks not implemented';
+          result.error = true;
+        }
+      } else if (pathElements[2] === 'depends') {
+        result.message = 'Get asset depends not implemented';
+        result.error = true;
+      } else {
+        result.message = `Unknown assets endpoint: [${pathElements.join()}]`;
+        result.error = true;
+      }
+      break;
+
+    // POST/PUT/DELETE assets/{assetname}/tasks/{taskPosition} OR
+    // GET /bedrock/assets/search/{searchString}
+    case 4:
+      if (pathElements[1] === 'search') {
+        result.message = 'Assets search not implemented';
+        result.error = true;
+      } else if (pathElements[2] === 'tasks') {
+        switch (verb) {
+          case 'POST':
+            result.message = 'Add asset task not implemented';
+            result.error = true;
+            break;
+
+          case 'PUT':
+            result.message = 'Update asset task not implemented';
+            result.error = true;
+            break;
+
+          case 'DELETE':
+            result.message = 'Delete asset task not implemented';
+            result.error = true;
+            break;
+
+          default:
+            result.message = `handleAssets: unknown verb ${verb}`;
+            result.error = true;
+            break;
+        }
+      } else {
+        result.message = `Unknown assets endpoint: [${pathElements.join()}]`;
+        result.error = true;
       }
       break;
 
     default:
-      ok = false;
-      message = `Unknown assets endpoint: ${JSON.stringify(pathElements)}`;
+      result.message = `Unknown assets endpoint: [${pathElements.join()}]`;
+      result.error = true;
       break;
   }
-  if (ok) {
-    return result;
+  if (result.error) {
+    console.log(result.message);
   }
-  throw message;
+  return result;
 }
 
 module.exports = handleAssets;
