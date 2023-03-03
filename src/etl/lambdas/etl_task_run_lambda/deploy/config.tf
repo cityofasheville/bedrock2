b8d4fd9062575ae2122a2a96c2619a8a765f4b5f
Jon Wilson <jontwilson@gmail.com>
2023-03-03 09:54:09 -0500

    New task type: Run an outside Lambda

---
+++ src/etl/lambdas/etl_task_run_lambda/deploy/config.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region	= var.region
+}
+
+resource "aws_lambda_function" "etl_task_run_lambda-$$INSTANCE$$" {
+    filename        = "function.zip"
+    function_name   = "etl_task_run_lambda-$$INSTANCE$$"
+    role            = data.terraform_remote_state.lambda_role.outputs.bedrock_lambda_role_arn
+    handler         = "handler.lambda_handler"
+    runtime         = "python3.8"
+    source_code_hash = filebase64sha256("function.zip")
+    timeout         = 900
+    tags = {
+      "coa:application" = "bedrock"
+      "coa:department"  = "information-technology"
+      "coa:owner"       = "jtwilson@ashevillenc.gov"
+      "coa:owner-team"  = "dev"
+    }
+    vpc_config {
+      subnet_ids         = var.subnet_ids
+      security_group_ids = var.security_group_ids
+    }
+}
+
+output "etl_task_run_lambda_arn" {
+  value = "${aws_lambda_function.etl_task_run_lambda-$$INSTANCE$$.arn}"
+}
