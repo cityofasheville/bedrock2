8cee1a8e86c8f2c2806a1eb3514b7c2032d8e618
Eric Jackson <ejackson@ashevillenc.gov>
2023-03-05 16:00:35 -0500

    Added a comment in handleRungroups

--- src/api/lambdas/bedrock-api-backend/handleRungroups.js
+++ src/api/lambdas/bedrock-api-backend/handleRungroups.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 function handleRungroups(event, pathElements, queryParams, verb) {
+  // This is just a placeholder:
   return {
     run_group_name: 'Monday-Morning-7AM',
     cron_string: '00 11 ? * MON *',
