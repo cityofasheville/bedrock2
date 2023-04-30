6c4a648f5b6c9b2e1a643b43b24fd5920118dcd7
Eric Jackson <ejackson@ashevillenc.gov>
2023-04-30 12:30:57 -0400

    Adding in the actual code.

--- src/api/lambdas/bedrock-api-backend/updateAsset.js
+++ src/api/lambdas/bedrock-api-backend/updateAsset.js
@@ -16,25 +16,32 @@ async function updateAsset(requestBody, pathElements, queryParams, connection) {
   if ('asset_name' in body && body.asset_name !== assetName) {
     result.error = true;
     result.message = `Asset name ${assetName} in path does not match asset name ${body.asset_name} in body`;
+    result.result = null;
     return result;
   }
 
   const client = new Client(connection);
   await client.connect()
     .catch((err) => {
-      console.log(JSON.stringify(err));
       const errmsg = pgErrorCodes[err.code];
-      throw new Error([`Postgres error: ${errmsg}`, err]);
+      console.log(JSON.stringify(err));
+      result.error = true;
+      result.message = `Postgres error: ${errmsg}`;
+      result.result = null;
     });
+
+  if (result.error) return result;
 
   // Verify that the asset exists
   let sql = 'SELECT * FROM bedrock.assets where asset_name like $1';
 
   let res = await client.query(sql, [assetName])
     .catch((err) => {
-      const errmsg = pgErrorCodes[err.code];
-      throw new Error([`Postgres error: ${errmsg}`, err]);
+      result.error = true;
+      result.message = `Postgres error: ${pgErrorCodes[err.code]}`;
+      result.result = null;
     });
+  if (result.error) return result;
 
   if (res.rowCount === 0) {
     result.error = true;
@@ -42,91 +49,102 @@ async function updateAsset(requestBody, pathElements, queryParams, connection) {
     await client.end();
   } else {
     let members = ['description', 'location', 'active'];
-    try {
-      await client.query('BEGIN');
+    await client.query('BEGIN');
 
-      // Start with the base asset
-      let cnt = 1;
-      let args = [];
-      sql = 'UPDATE assets SET ';
-      let comma = '';
-      for (let i = 0; i < members.length; i += 1) {
-        if (members[i] in body) {
-          sql += `${comma} ${members[i]} = $${cnt}`;
-          comma = ',';
-          cnt += 1;
-          args.push(body[members[i]]);
-          result.result[members[i]] = body[members[i]];
-        }
+    // Start with the base asset
+    let cnt = 1;
+    let args = [];
+    sql = 'UPDATE assets SET ';
+    let comma = '';
+    for (let i = 0; i < members.length; i += 1) {
+      if (members[i] in body) {
+        sql += `${comma} ${members[i]} = $${cnt}`;
+        comma = ',';
+        cnt += 1;
+        args.push(body[members[i]]);
+        result.result[members[i]] = body[members[i]];
       }
-      sql += ` where asset_name = $${cnt}`;
-      args.push(assetName);
-      res = await client.query(sql, args)
-        .catch((err) => {
-          const errmsg = pgErrorCodes[err.code];
-          throw new Error([`Postgres error: ${errmsg}`, err]);
-        });
+    }
+    sql += ` where asset_name = $${cnt}`;
+    args.push(assetName);
+    res = await client.query(sql, args)
+      .catch((err) => {
+        result.error = true;
+        result.message = `Postgres error: ${pgErrorCodes[err.code]}`;
+        result.result = null;
+      });
+    if (result.error) {
+      await client.query('ROLLBACK');
+      await client.end();
+      return result;
+    }
 
-      // Now add any dependencies, always replacing existing with new
-      if (('dependencies' in body)) {
-        res = await client.query('DELETE FROM dependencies WHERE asset_name = $1', [assetName]);
-        if (body.dependencies.length > 0) {
-          for (let i = 0; i < body.dependencies.length; i += 1) {
-            const dependency = body.dependencies[i];
-            // eslint-disable-next-line no-await-in-loop
-            res = await client.query(
-              'INSERT INTO dependencies (asset_name, dependency) VALUES ($1, $2)',
-              [assetName, dependency],
-            )
-              .catch((err) => {
-                const errmsg = pgErrorCodes[err.code];
-                throw new Error([`Postgres error inserting asset dependencies: ${errmsg}`, err]);
-              });
-          }
-        }
-        result.result.dependencies = body.dependencies;
-      }
-
-      // Now add any ETL information. Null run group means delete
-      if (('etl_run_group' in body || 'etl_active' in body)) {
-        if ('etl_run_group' in body && body.etl_run_group === null) { // Delete the ETL information
-          await client.query('DELETE FROM etl where asset_name = $1', [assetName]);
-          await client.query('DELETE FROM tasks where asset_name = $1', [assetName]);
-        } else { // Update the ETL information
-          members = ['etl_run_group', 'etl_active'];
-          cnt = 1;
-          args = [];
-          sql = 'UPDATE etl SET ';
-          comma = '';
-          for (let i = 0; i < members.length; i += 1) {
-            if (members[i] in body) {
-              sql += `${comma} ${members[i].substring(4)} = $${cnt}`;
-              comma = ',';
-              cnt += 1;
-              args.push(body[members[i]]);
-              result.result[members[i]] = body[members[i]];
-            }
-          }
-          sql += ` where asset_name = $${cnt}`;
-          args.push(assetName);
-          console.log(sql);
-          await client.query(sql, args)
+    // Now add any dependencies, always replacing existing with new
+    if (('dependencies' in body)) {
+      res = await client.query('DELETE FROM dependencies WHERE asset_name = $1', [assetName]);
+      if (body.dependencies.length > 0) {
+        for (let i = 0; i < body.dependencies.length; i += 1) {
+          const dependency = body.dependencies[i];
+          // eslint-disable-next-line no-await-in-loop
+          res = await client.query(
+            'INSERT INTO dependencies (asset_name, dependency) VALUES ($1, $2)',
+            [assetName, dependency],
+          )
             .catch((err) => {
-              const errmsg = pgErrorCodes[err.code];
-              throw new Error([`Postgres error inserting asset etl info: ${errmsg}`, err]);
+              result.error = true;
+              result.message = `Postgres error: ${pgErrorCodes[err.code]}`;
+              result.result = null;
             });
         }
       }
-      await client.query('COMMIT');
-    } catch (e) {
-      result.error = true;
-      result.message = e;
-      await client.query('ROLLBACK');
-    } finally {
-      await client.end();
+      result.result.dependencies = body.dependencies;
     }
+    if (result.error) {
+      await client.query('ROLLBACK');
+      await client.end();
+      return result;
+    }
+
+    // Now add any ETL information. Null run group means delete
+    if (('etl_run_group' in body || 'etl_active' in body)) {
+      if ('etl_run_group' in body && body.etl_run_group === null) { // Delete the ETL information
+        await client.query('DELETE FROM etl where asset_name = $1', [assetName]);
+        await client.query('DELETE FROM tasks where asset_name = $1', [assetName]);
+      } else { // Update the ETL information
+        members = ['etl_run_group', 'etl_active'];
+        cnt = 1;
+        args = [];
+        sql = 'UPDATE etl SET ';
+        comma = '';
+        for (let i = 0; i < members.length; i += 1) {
+          if (members[i] in body) {
+            sql += `${comma} ${members[i].substring(4)} = $${cnt}`;
+            comma = ',';
+            cnt += 1;
+            args.push(body[members[i]]);
+            result.result[members[i]] = body[members[i]];
+          }
+        }
+        sql += ` where asset_name = $${cnt}`;
+        args.push(assetName);
+        console.log(sql);
+        await client.query(sql, args)
+          .catch((err) => {
+            result.error = true;
+            result.message = `Postgres error: ${pgErrorCodes[err.code]}`;
+            result.result = null;
+          });
+      }
+      if (result.error) {
+        await client.query('ROLLBACK');
+        await client.end();
+        return result;
+      }
+      await client.query('COMMIT');
+    }
+    console.log('Returning result: ', JSON.stringify(result));
+    return result;
   }
-  return result;
 }
 
 module.exports = updateAsset;
