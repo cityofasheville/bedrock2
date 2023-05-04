aa7adfb41addf25cb352aa17ba4d3d18ececdf7a
Eric Jackson <ejaxon@users.noreply.github.com>
2023-05-04 11:42:10 -0400

    Assets cleanup (#130)
    * Cleaned up error handling in asset functions
    * Made all multi-table functions transational

--- src/api/lambdas/bedrock-api-backend/getAsset.js
+++ src/api/lambdas/bedrock-api-backend/getAsset.js
@@ -3,31 +3,38 @@ const { Client } = require('pg');
 const pgErrorCodes = require('./pgErrorCodes');
 
 async function getAsset(pathElements, queryParams, connection) {
+  let res;
   const result = {
     error: false,
     message: '',
-    result: null,
+    result: {},
   };
 
   const client = new Client(connection);
   await client.connect()
     .catch((err) => {
-      console.log(JSON.stringify(err));
-      const errmsg = pgErrorCodes[err.code];
-      throw new Error([`Postgres error: ${errmsg}`, err]);
+      result.error = true;
+      result.message = `PG error connecting: ${pgErrorCodes[err.code]}`;
     });
 
-  const sql = `SELECT a.*, e.run_group, e.active, d.dependency
-  FROM bedrock.assets a 
-  left join bedrock.etl e on e.asset_name = a.asset_name
-  left join bedrock.dependencies d on d.asset_name = a.asset_name
-  where a.asset_name like $1`;
+  if (!result.error) {
+    const sql = `SELECT a.*, e.run_group, e.active, d.dependency
+    FROM bedrock.assets a 
+    left join bedrock.etl e on e.asset_name = a.asset_name
+    left join bedrock.dependencies d on d.asset_name = a.asset_name
+    where a.asset_name like $1`;
 
-  let res = await client.query(sql, [pathElements[1]])
-    .catch((err) => {
-      const errmsg = pgErrorCodes[err.code];
-      throw new Error([`Postgres error: ${errmsg}`, err]);
-    });
+    res = await client.query(sql, [pathElements[1]])
+      .catch((err) => {
+        result.error = true;
+        result.message = `PG error getting asset information: ${pgErrorCodes[err.code]}`;
+      });
+  }
+
+  if (result.error) {
+    result.result = null;
+    return result;
+  }
 
   if (res.rowCount === 0) {
     result.error = true;
@@ -41,7 +48,7 @@ async function getAsset(pathElements, queryParams, connection) {
       etl_run_group: res.rows[0].run_group,
       etl_active: res.rows[0].active,
       dependencies: [],
-      tags: []
+      tags: [],
     };
     for (let i = 0; i < res.rowCount; i += 1) {
       if (res.rows[i].dependency !== null) {
@@ -53,11 +60,12 @@ async function getAsset(pathElements, queryParams, connection) {
   // Now get tags
   res = await client.query('SELECT * from bedrock.asset_tags where asset_name like $1', [pathElements[1]])
     .catch((err) => {
-      const errmsg = pgErrorCodes[err.code];
-      throw new Error([`Postgres error: ${errmsg}`, err]);
+      result.error = true;
+      result.message = `PG error getting asset_tags: ${pgErrorCodes[err.code]}`;
+      result.result = null;
     });
 
-  if (res.rowCount > 0) {
+  if (!result.error && res.rowCount > 0) {
     for (let i = 0; i < res.rowCount; i += 1) {
       if (res.rows[i].tag_name !== null) {
         result.result.tags.push(res.rows[i].tag_name);
--- src/api/lambdas/bedrock-api-backend/updateAsset.js
+++ src/api/lambdas/bedrock-api-backend/updateAsset.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 /* eslint-disable no-console */
 const { Client } = require('pg');
 const pgErrorCodes = require('./pgErrorCodes');
@@ -16,7 +17,6 @@ async function updateAsset(requestBody, pathElements, queryParams, connection) {
   if ('asset_name' in body && body.asset_name !== assetName) {
     result.error = true;
     result.message = `Asset name ${assetName} in path does not match asset name ${body.asset_name} in body`;
-    result.result = null;
     return result;
   }
 
@@ -26,7 +26,7 @@ async function updateAsset(requestBody, pathElements, queryParams, connection) {
       const errmsg = pgErrorCodes[err.code];
       console.log(JSON.stringify(err));
       result.error = true;
-      result.message = `Postgres error: ${errmsg}`;
+      result.message = `PG error connecting: ${errmsg}`;
       result.result = null;
     });
 
@@ -38,180 +38,201 @@ async function updateAsset(requestBody, pathElements, queryParams, connection) {
   let res = await client.query(sql, [assetName])
     .catch((err) => {
       result.error = true;
-      result.message = `Postgres error: ${pgErrorCodes[err.code]}`;
+      result.message = `PG error verifying that asset exists: ${pgErrorCodes[err.code]}`;
       result.result = null;
     });
-  if (result.error) return result;
 
-  if (res.rowCount === 0) {
+  if (res.rowCount === 0 && !result.error) {
     result.error = true;
     result.message = `Asset ${assetName} does not exist`;
-    await client.end();
-  } else {
-    let members = ['description', 'location', 'active'];
-    await client.query('BEGIN');
+  }
 
-    // Start with the base asset
-    let cnt = 1;
-    let args = [];
-    sql = 'UPDATE assets SET ';
-    let comma = '';
-    for (let i = 0; i < members.length; i += 1) {
-      if (members[i] in body) {
-        sql += `${comma} ${members[i]} = $${cnt}`;
-        comma = ',';
-        cnt += 1;
-        args.push(body[members[i]]);
-        result.result[members[i]] = body[members[i]];
-      }
+  if (result.error) {
+    await client.end();
+    return result;
+  }
+
+  //
+  // Now begin the transaction
+  //
+  await client.query('BEGIN');
+
+  // Start with the base asset
+  let members = ['description', 'location', 'active'];
+  let cnt = 1;
+  let args = [];
+  sql = 'UPDATE assets SET ';
+
+  for (let i = 0, comma = ''; i < members.length; i += 1, comma = ',', cnt += 1) {
+    if (members[i] in body) {
+      sql += `${comma} ${members[i]} = $${cnt}`;
+      args.push(body[members[i]]);
+      result.result[members[i]] = body[members[i]];
     }
-    sql += ` where asset_name = $${cnt}`;
-    args.push(assetName);
-    res = await client.query(sql, args)
+  }
+  sql += ` where asset_name = $${cnt}`;
+  args.push(assetName);
+  res = await client.query(sql, args)
+    .catch((err) => {
+      result.error = true;
+      result.message = `PG error updating base asset: ${pgErrorCodes[err.code]}`;
+      result.result = null;
+    });
+
+  // Now add any dependencies, always replacing existing with new
+  if (!result.error && 'dependencies' in body) {
+    res = await client.query('DELETE FROM dependencies WHERE asset_name = $1', [assetName])
       .catch((err) => {
         result.error = true;
-        result.message = `Postgres error: ${pgErrorCodes[err.code]}`;
+        result.message = `PG error deleting dependencies for update: ${pgErrorCodes[err.code]}`;
         result.result = null;
       });
-    if (result.error) {
-      await client.query('ROLLBACK');
-      await client.end();
-      return result;
+
+    if (body.dependencies.length > 0 && !result.error) {
+      for (let i = 0; i < body.dependencies.length && !result.error; i += 1) {
+        const dependency = body.dependencies[i];
+        res = await client.query(
+          'INSERT INTO dependencies (asset_name, dependency) VALUES ($1, $2)',
+          [assetName, dependency],
+        )
+          .catch((err) => {
+            result.error = true;
+            result.message = `PG error updating dependencies: ${pgErrorCodes[err.code]}`;
+            result.result = null;
+          });
+      }
+    }
+    result.result.dependencies = body.dependencies;
+  }
+  if (result.error) {
+    await client.query('ROLLBACK');
+    await client.end();
+    return result;
+  }
+
+  // Now add any ETL information. Null run group means delete
+  if (!result.error && ('etl_run_group' in body || 'etl_active' in body)) {
+    if ('etl_run_group' in body && body.etl_run_group === null) { // Delete the ETL information
+      await client.query('DELETE FROM etl where asset_name = $1', [assetName])
+        .catch((err) => {
+          result.error = true;
+          result.message = `PG error deleting from etl for update: ${pgErrorCodes[err.code]}`;
+          result.result = null;
+        });
+      if (!result.error) {
+        await client.query('DELETE FROM tasks where asset_name = $1', [assetName])
+          .catch((err) => {
+            result.error = true;
+            result.message = `PG error deleting from tasks for update: ${pgErrorCodes[err.code]}`;
+            result.result = null;
+          });
+      }
+    } else { // Update the ETL information
+      members = ['etl_run_group', 'etl_active'];
+      cnt = 1;
+      args = [];
+      sql = 'UPDATE etl SET ';
+      for (let i = 0, comma = ''; i < members.length; i += 1, comma = ',', cnt += 1) {
+        if (members[i] in body) {
+          sql += `${comma} ${members[i].substring(4)} = $${cnt}`;
+          args.push(body[members[i]]);
+          result.result[members[i]] = body[members[i]];
+        }
+      }
+      sql += ` where asset_name = $${cnt}`;
+      args.push(assetName);
+      await client.query(sql, args)
+        .catch((err) => {
+          result.error = true;
+          result.message = `PG error updating etl: ${pgErrorCodes[err.code]}`;
+          result.result = null;
+        });
+    }
+  }
+
+  // Finally, update any tags.
+  if (!result.error && 'tags' in body) {
+    const tags = []; let tmpTags = [];
+    if (Array.isArray(body.tags)) {
+      tmpTags = body.tags;
+    } else {
+      tmpTags = body.tags.split(',');
     }
 
-    // Now add any dependencies, always replacing existing with new
-    if (('dependencies' in body)) {
-      res = await client.query('DELETE FROM dependencies WHERE asset_name = $1', [assetName]);
-      if (body.dependencies.length > 0) {
-        for (let i = 0; i < body.dependencies.length; i += 1) {
-          const dependency = body.dependencies[i];
-          // eslint-disable-next-line no-await-in-loop
-          res = await client.query(
-            'INSERT INTO dependencies (asset_name, dependency) VALUES ($1, $2)',
-            [assetName, dependency],
+    for (let i = 0; i < tmpTags.length; i += 1) {
+      const tag = tmpTags[i].trim();
+      if (tag.length > 0) {
+        tags.push(tag); // Make sure they're cleaned up
+      }
+    }
+    // For now, just add any tags that aren't in the tags table
+    sql = 'SELECT tag_name from bedrock.tags where tag_name in (';
+    cnt = 1;
+    for (let i = 0, comma = ''; i < tags.length; i += 1, comma = ', ', cnt += 1) {
+      sql += `${comma}$${cnt}`;
+    }
+    sql += ');';
+    res = await client.query(sql, tags)
+      .catch((err) => {
+        result.error = true;
+        result.message = `PG error reading tags for update: ${pgErrorCodes[err.code]}`;
+        result.result = null;
+      });
+
+    if (!result.error && res.rowCount !== tags.length) {
+      const dbTags = [];
+      for (let i = 0; i < res.rowCount; i += 1) {
+        dbTags.push(res.rows[i].tag_name);
+      }
+      for (let i = 0; i < tags.length && !result.error; i += 1) {
+        if (!dbTags.includes(tags[i])) {
+          await client.query(
+            'INSERT INTO tags (tag_name) VALUES ($1)',
+            [tags[i]],
           )
             .catch((err) => {
               result.error = true;
-              result.message = `Postgres error: ${pgErrorCodes[err.code]}`;
+              result.message = `PG error adding tags to tag table for update: ${pgErrorCodes[err.code]}`;
               result.result = null;
             });
         }
       }
-      result.result.dependencies = body.dependencies;
     }
-    if (result.error) {
-      await client.query('ROLLBACK');
-      await client.end();
-      return result;
-    }
+    // End of adding any tags that aren't in the tags table for now
 
-    // Now add any ETL information. Null run group means delete
-    if (('etl_run_group' in body || 'etl_active' in body)) {
-      if ('etl_run_group' in body && body.etl_run_group === null) { // Delete the ETL information
-        await client.query('DELETE FROM etl where asset_name = $1', [assetName]);
-        await client.query('DELETE FROM tasks where asset_name = $1', [assetName]);
-      } else { // Update the ETL information
-        members = ['etl_run_group', 'etl_active'];
-        cnt = 1;
-        args = [];
-        sql = 'UPDATE etl SET ';
-        comma = '';
-        for (let i = 0; i < members.length; i += 1) {
-          if (members[i] in body) {
-            sql += `${comma} ${members[i].substring(4)} = $${cnt}`;
-            comma = ',';
-            cnt += 1;
-            args.push(body[members[i]]);
-            result.result[members[i]] = body[members[i]];
-          }
-        }
-        sql += ` where asset_name = $${cnt}`;
-        args.push(assetName);
-        console.log(sql);
-        await client.query(sql, args)
-          .catch((err) => {
-            result.error = true;
-            result.message = `Postgres error: ${pgErrorCodes[err.code]}`;
-            result.result = null;
-          });
-      }
-      if (result.error) {
-        await client.query('ROLLBACK');
-        await client.end();
-        return result;
-      }
-      await client.query('COMMIT');
-    }
-
-    // Finally, update any tags.
-    if ('tags' in body) {
-      const tags = []; let tmpTags = [];
-      if (Array.isArray(body.tags)) {
-        tmpTags = body.tags;
-      } else {
-        tmpTags = body.tags.split(',');
-      }
-
-      for (let i = 0; i < tmpTags.length; i += 1) {
-        const tag = tmpTags[i].trim();
-        if (tag.length > 0) {
-          tags.push(tag); // Make sure they're cleaned up
-        }
-      }
-      // For now, just add any tags that aren't in the tags table
-      sql = 'SELECT tag_name from bedrock.tags where tag_name in (';
-      cnt = 1;
-      comma = '';
-      for (let i = 0; i < tags.length; i += 1) {
-        sql += `${comma}$${cnt}`;
-        cnt += 1;
-        comma = ', ';
-      }
-      sql += ');';
-      res = await client.query(sql, tags)
+    // Now delete any existing tags
+    if (!result.error) {
+      await client.query('DELETE FROM bedrock.asset_tags where asset_name = $1', [assetName])
         .catch((err) => {
-          const errmsg = pgErrorCodes[err.code];
-          throw new Error([`Postgres error reading asset tags: ${errmsg}`, err]);
+          result.error = true;
+          result.message = `PG error deleting tags for update: ${pgErrorCodes[err.code]}`;
+          result.result = null;
         });
-      if (res.rowCount !== tags.length) {
-        const dbTags = [];
-        for (let i = 0; i < res.rowCount; i += 1) {
-          dbTags.push(res.rows[i].tag_name);
-        }
-        for (let i = 0; i < tags.length; i += 1) {
-          if (!dbTags.includes(tags[i])) {
-            // eslint-disable-next-line no-await-in-loop
-            await client.query(
-              'INSERT INTO tags (tag_name) VALUES ($1)',
-              [tags[i]],
-            )
-              .catch((err) => {
-                const errmsg = pgErrorCodes[err.code];
-                throw new Error([`Postgres error inserting asset tags: ${errmsg}`, err]);
-              });
-          }
-        }
-      }
-      // End of just adding any tags that aren't in the tags table for now
-      // Now delete any existing tags
-      await client.query('DELETE FROM bedrock.asset_tags where asset_name = $1', [assetName]);
-
-      // And add the new ones back in
-      for (let i = 0; i < tags.length; i += 1) {
-        // eslint-disable-next-line no-await-in-loop
-        res = await client.query(
-          'INSERT INTO bedrock.asset_tags (asset_name, tag_name) VALUES ($1, $2)',
-          [body.asset_name, tags[i]],
-        )
-          .catch((err) => {
-            const errmsg = pgErrorCodes[err.code];
-            throw new Error([`Postgres error adding asset tags: ${errmsg}`, err]);
-          });
-      }
-      result.result.tags = body.tags;
     }
-    return result;
+
+    // And add the new ones back in
+    for (let i = 0; i < tags.length && !result.error; i += 1) {
+      res = await client.query(
+        'INSERT INTO bedrock.asset_tags (asset_name, tag_name) VALUES ($1, $2)',
+        [body.asset_name, tags[i]],
+      )
+        .catch((err) => {
+          result.error = true;
+          result.message = `PG error inserting tags for update: ${pgErrorCodes[err.code]}`;
+          result.result = null;
+        });
+    }
+    result.result.tags = body.tags;
   }
+
+  if (result.error) {
+    await client.query('ROLLBACK');
+    result.result = null;
+  } else {
+    await client.query('COMMIT');
+  }
+  await client.end();
+
   return result;
 }
 
