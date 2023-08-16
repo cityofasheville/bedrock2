ed2024c21198b9fb281ceadaf9c65d103b0e5083
Eric Jackson <ejackson@ashevillenc.gov>
2023-08-16 18:35:12 -0400

    Added fields parameter for asset GET to select which fields to return

--- src/api/lambdas/bedrock-api-backend/getAsset.js
+++ src/api/lambdas/bedrock-api-backend/getAsset.js
@@ -4,6 +4,7 @@ const pgErrorCodes = require('./pgErrorCodes');
 
 async function getAsset(pathElements, queryParams, connection) {
   let res;
+  let fields = null;
   const result = {
     error: false,
     message: '',
@@ -40,37 +41,67 @@ async function getAsset(pathElements, queryParams, connection) {
     result.error = true;
     result.message = 'Asset not found';
   } else {
-    result.result = {
-      asset_name: res.rows[0].asset_name,
-      description: res.rows[0].description,
-      location: res.rows[0].location,
-      owner_id: res.rows[0].owner_id,
-      notes: res.rows[0].notes,
-      active: res.rows[0].active,
-      etl_run_group: res.rows[0].run_group,
-      etl_active: res.rows[0].etl_active,
-      dependencies: [],
-      tags: [],
-    };
-    for (let i = 0; i < res.rowCount; i += 1) {
-      if (res.rows[i].dependency !== null) {
-        result.result.dependencies.push(res.rows[i].dependency);
+    const available = ['description', 'location', 'active', 'owner_id', 'notes', 'tags', 'dependencies', 'etl_run_group', 'etl_active'];
+    if ('fields' in queryParams) {
+      fields = queryParams.fields.split(',');
+      result.result = {
+        asset_name: res.rows[0].asset_name,
+      };
+      // eslint-disable-next-line guard-for-in, no-restricted-syntax
+      for (const idx in fields) {
+        const itm = fields[idx];
+        if (available.includes(itm)) {
+          if (itm === 'dependencies') {
+            result.result.dependencies = [];
+            for (let i = 0; i < res.rowCount; i += 1) {
+              if (res.rows[i].dependency !== null) {
+                result.result.dependencies.push(res.rows[i].dependency);
+              }
+            }
+          } else if (itm === 'etl_run_group') {
+            result.result[itm] = res.rows[0].run_group;
+          } else if (itm === 'tags') {
+            result.result[itm] = []; // See below
+          } else {
+            result.result[itm] = res.rows[0][itm];
+          }
+        }
+      }
+    } else {
+      result.result = {
+        asset_name: res.rows[0].asset_name,
+        description: res.rows[0].description,
+        location: res.rows[0].location,
+        owner_id: res.rows[0].owner_id,
+        notes: res.rows[0].notes,
+        active: res.rows[0].active,
+        etl_run_group: res.rows[0].run_group,
+        etl_active: res.rows[0].etl_active,
+        dependencies: [],
+        tags: [],
+      };
+      for (let i = 0; i < res.rowCount; i += 1) {
+        if (res.rows[i].dependency !== null) {
+          result.result.dependencies.push(res.rows[i].dependency);
+        }
       }
     }
   }
 
-  // Now get tags
-  res = await client.query('SELECT * from bedrock.asset_tags where asset_name like $1', [pathElements[1]])
-    .catch((err) => {
-      result.error = true;
-      result.message = `PG error getting asset_tags: ${pgErrorCodes[err.code]}`;
-      result.result = null;
-    });
+  if (fields === null || fields.includes('tags')) {
+    // Now get tags
+    res = await client.query('SELECT * from bedrock.asset_tags where asset_name like $1', [pathElements[1]])
+      .catch((err) => {
+        result.error = true;
+        result.message = `PG error getting asset_tags: ${pgErrorCodes[err.code]}`;
+        result.result = null;
+      });
 
-  if (!result.error && res.rowCount > 0) {
-    for (let i = 0; i < res.rowCount; i += 1) {
-      if (res.rows[i].tag_name !== null) {
-        result.result.tags.push(res.rows[i].tag_name);
+    if (!result.error && res.rowCount > 0) {
+      for (let i = 0; i < res.rowCount; i += 1) {
+        if (res.rows[i].tag_name !== null) {
+          result.result.tags.push(res.rows[i].tag_name);
+        }
       }
     }
   }
