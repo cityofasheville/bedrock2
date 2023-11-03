66687be2f871d136e1856cc42768054f06af2088
Cameron Henshaw <cameronhenshaw@Camerons-MBP.lan>
2023-11-02 22:05:23 -0400

    cleanup

--- src/api/lambdas/bedrock-api-backend/getAsset.js
+++ src/api/lambdas/bedrock-api-backend/getAsset.js
@@ -2,34 +2,146 @@
 const { Client } = require('pg');
 const pgErrorCodes = require('./pgErrorCodes');
 
-async function getAsset(pathElements, queryParams, connection) {
+async function newClient(connection) {
+  const client = new Client(connection);
+  try {
+    await client.connect();
+    return client;
+  } catch (error) {
+    throw new Error(`PG error connecting: ${pgErrorCodes[error.code]}`);
+  }
+}
+
+async function readAsset(client, pathElements) {
   let res;
-  let fields = null;
-  const result = {
+  const sql = `SELECT a.*, e.run_group, e.active as etl_active, d.dependency
+    FROM bedrock.assets a
+    left join bedrock.etl e on e.asset_name = a.asset_name
+    left join bedrock.dependencies d on d.asset_name = a.asset_name
+    where a.asset_name like $1`;
+  try {
+    res = await client.query(sql, [pathElements[1]]);
+  } catch (error) {
+    throw new Error(
+      `PG error getting asset information: ${pgErrorCodes[error.code]}`,
+    );
+  }
+  console.log(`logging res ${res}`);
+  if (res.rowCount === 0) {
+    throw new Error('Asset not found');
+  }
+  return res.rows;
+}
+
+async function addInfo(res, fields, available) {
+  const info = {
+    error: false,
+    message: '',
+    result: {},
+  };
+  info.result = {
+    asset_name: res[0].asset_name,
+  };
+  for (let j = 0; j < fields.length; ++j) {
+    const itm = fields[j];
+    if (available.includes(itm)) {
+      if (itm === 'dependencies') {
+        info.result.dependencies = [];
+        for (let i = 0; i < res.length; i += 1) {
+          if (res[i].dependency !== null) {
+            info.result.dependencies.push(res[i].dependency);
+          }
+        }
+      } else if (itm === 'etl_run_group') {
+        info.result[itm] = res[0].run_group;
+      } else if (itm === 'tags') {
+        info.result[itm] = [];
+      } else {
+        info.result[itm] = res[0][itm];
+      }
+    }
+  }
+  return info;
+}
+
+async function getTags(client, pathElements) {
+  const res = await client
+    .query('SELECT * from bedrock.asset_tags where asset_name like $1', [
+      pathElements[1],
+    ])
+    .catch((error) => {
+      throw new Error(
+        `PG error getting asset_tags: ${pgErrorCodes[error.code]}`,
+      );
+    });
+  await client.end();
+  return res;
+}
+
+async function getAsset(pathElements, queryParams, connection) {
+  let result = {
     error: false,
     message: '',
     result: {},
   };
 
-  const client = new Client(connection);
-  await client.connect().catch((err) => {
+  let client;
+  try {
+    client = await newClient(connection);
+  } catch (error) {
     result.error = true;
-    result.message = `PG error connecting: ${pgErrorCodes[err.code]}`;
-  });
+    result.message = error.message;
+    result.result = null;
+    return result;
+  }
 
-  if (!result.error) {
-    const sql = `SELECT a.*, e.run_group, e.active as etl_active, d.dependency
-    FROM bedrock.assets a 
-    left join bedrock.etl e on e.asset_name = a.asset_name
-    left join bedrock.dependencies d on d.asset_name = a.asset_name
-    where a.asset_name like $1`;
+  let res;
+  try {
+    res = await readAsset(client, pathElements);
+  } catch (error) {
+    result.error = true;
+    result.message = error.message;
+    result.result = null;
+    return result;
+  }
 
-    res = await client.query(sql, [pathElements[1]]).catch((err) => {
+  let fields = null;
+  const available = [
+    'description',
+    'location',
+    'active',
+    'owner_id',
+    'notes',
+    'tags',
+    'dependencies',
+    'etl_run_group',
+    'etl_active',
+  ];
+  // Use fields from the query if they're present, otherwise use all available fields
+  if ('fields' in queryParams) {
+    fields = queryParams.fields.replace('[', '').replace(']', '').split(',');
+  } else {
+    fields = [...available];
+  }
+
+  result = await addInfo(res, fields, available);
+  if (fields === null || fields.includes('tags')) {
+    try {
+      res = await getTags(client, pathElements);
+    } catch (error) {
       result.error = true;
-      result.message = `PG error getting asset information: ${
-        pgErrorCodes[err.code]
-      }`;
-    });
+      result.message = error.message;
+      result.result = null;
+      return result;
+    }
+  }
+
+  if (res.rowCount > 0) {
+    for (let i = 0; i < res.rowCount; i += 1) {
+      if (res.rows[i].tag_name !== null) {
+        result.result.tags.push(res.rows[i].tag_name);
+      }
+    }
   }
 
   if (result.error) {
@@ -37,74 +149,6 @@ async function getAsset(pathElements, queryParams, connection) {
     return result;
   }
 
-  if (res.rowCount === 0) {
-    result.error = true;
-    result.message = 'Asset not found';
-  } else {
-    const available = [
-      'description',
-      'location',
-      'active',
-      'owner_id',
-      'notes',
-      'tags',
-      'dependencies',
-      'etl_run_group',
-      'etl_active',
-    ];
-    if ('fields' in queryParams) {
-      fields = queryParams.fields.split(',');
-    } else {
-      fields = [...available];
-    }
-    result.result = {
-      asset_name: res.rows[0].asset_name,
-    };
-    // eslint-disable-next-line guard-for-in, no-restricted-syntax
-    for (const idx in fields) {
-      const itm = fields[idx];
-      if (available.includes(itm)) {
-        if (itm === 'dependencies') {
-          result.result.dependencies = [];
-          for (let i = 0; i < res.rowCount; i += 1) {
-            if (res.rows[i].dependency !== null) {
-              result.result.dependencies.push(res.rows[i].dependency);
-            }
-          }
-        } else if (itm === 'etl_run_group') {
-          result.result[itm] = res.rows[0].run_group;
-        } else if (itm === 'tags') {
-          result.result[itm] = [];
-        } else {
-          result.result[itm] = res.rows[0][itm];
-        }
-      }
-    }
-  }
-
-  if (fields === null || fields.includes('tags')) {
-    // Now get tags
-    res = await client
-      .query('SELECT * from bedrock.asset_tags where asset_name like $1', [
-        pathElements[1],
-      ])
-      .catch((err) => {
-        result.error = true;
-        result.message = `PG error getting asset_tags: ${
-          pgErrorCodes[err.code]
-        }`;
-        result.result = null;
-      });
-
-    if (!result.error && res.rowCount > 0) {
-      for (let i = 0; i < res.rowCount; i += 1) {
-        if (res.rows[i].tag_name !== null) {
-          result.result.tags.push(res.rows[i].tag_name);
-        }
-      }
-    }
-  }
-  await client.end();
   return result;
 }
 
