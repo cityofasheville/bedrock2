7cae3cb740cfa373d21b148238bb36940eb6029c
Eric Jackson <ejackson@ashevillenc.gov>
2023-11-24 11:51:32 -0500

    Update function.zip build for etl_task_table_copy

--- src/etl/lambdas/etl_task_table_copy/deploy/config.tf
+++ src/etl/lambdas/etl_task_table_copy/deploy/config.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "etl_task_table_copy-$$INSTANCE$$" {
-    filename        = "function.zip"
+    filename        = "../function.zip"
     function_name   = "etl_task_table_copy-$$INSTANCE$$"
     role            = data.terraform_remote_state.lambda_role.outputs.bedrock_lambda_role_arn
     handler         = "handler.lambda_handler"
     runtime         = "nodejs20.x"
-    source_code_hash = filebase64sha256("function.zip")
+    source_code_hash = filebase64sha256("../function.zip")
         layers = [
       data.terraform_remote_state.bedrock_common_$$INSTANCE$$.outputs.bedrock_common_$$INSTANCE$$_layer_arn,
       data.terraform_remote_state.bedrock_table_copy_$$INSTANCE$$.outputs.bedrock_table_copy_$$INSTANCE$$_layer_arn
