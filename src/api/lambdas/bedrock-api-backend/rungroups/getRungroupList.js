d86f40a40afa63bbb3d11e89607dc7ea1c90077c
Cameron Henshaw <cameronhenshaw@Camerons-MBP.lan>
2023-11-28 17:25:24 -0500

    Cleaning up getRungroupList file

--- src/api/lambdas/bedrock-api-backend/rungroups/getRungroupList.js
+++ src/api/lambdas/bedrock-api-backend/rungroups/getRungroupList.js
@@ -2,95 +2,152 @@
 const { Client } = require('pg');
 const pgErrorCodes = require('../pgErrorCodes');
 
-async function getRungroupList(domainName, pathElements, queryParams, connection) {
-  let offset = 0;
-  let count = 25;
-  let total = -1;
-  let where = ' where';
-  let qPrefix = '?';
-  let qParams = '';
-  const result = {
-    error: false,
-    message: '',
-    result: null,
-  };
-  const client = new Client(connection);
-  await client.connect().catch((err) => {
-    console.log(JSON.stringify(err));
-    const errmsg = pgErrorCodes[err.code];
-    throw new Error([`Postgres error: ${errmsg}`, err]);
-  });
+function checkParameters(queryParams) {
+  const parameters = ['period', 'pattern'];
+  let message = '';
 
-  // Override start, count, or offset, if set in query
+  Object.keys(queryParams).forEach((key) => {
+    if (key === 'period') {
+      message += 'Query parameter period not yet implemented. ';
+    } else if (!parameters.includes(key)) {
+      message += `${key} is not a valid parameter.`;
+    }
+  });
+  return message;
+}
+
+async function newClient(connection) {
+  const client = new Client(connection);
+  try {
+    await client.connect();
+    return client;
+  } catch (error) {
+    throw new Error(`PG error connecting: ${pgErrorCodes[error.code]}`);
+  }
+}
+
+function buildOffset(queryParams) {
+  let offset = 0;
   if ('offset' in queryParams) {
     offset = queryParams.offset;
   }
+  return offset;
+}
+
+function buildCount(queryParams) {
+  let count = 25;
   if ('count' in queryParams) {
     count = queryParams.count;
-    qParams += `${qPrefix}count=${count}`;
-    qPrefix = '&';
   }
+  return count;
+}
 
-  // Read the DB
-  const sqlParams = [];
-  let sql2 = '';
+function buildWhereClause(queryParams) {
+  const whereClause = {
+    whereClause: '',
+    sqlParams: [],
+  };
+  const where = ' where';
+  whereClause.whereClause = '';
+
   if ('pattern' in queryParams) {
-    const { pattern } = queryParams;
-    sql2 += `${where} run_group_name like $1`;
-    where = ' ';
-    sqlParams.push(`%${queryParams.pattern}%`);
-    qParams += `${qPrefix}pattern=${pattern}`;
-    qPrefix = '&';
+    whereClause.whereClause += `${where} run_group_name like $1`;
+    whereClause.sqlParams.push(`%${queryParams.pattern}%`);
   }
-  if ('period' in queryParams) {
-    qParams += `${qPrefix}period=${queryParams.period}`;
-    qPrefix = '&';
-    result.message += 'Query parameter period not yet implemented. ';
-  }
-  let sql = `SELECT count(*) FROM bedrock.run_groups  ${sql2}`;
-  console.log('run sql1 = ', sql);
-  let res = await client.query(sql, sqlParams).catch((err) => {
-    const errmsg = pgErrorCodes[err.code];
-    console.log(err, errmsg);
-    throw new Error([`Postgres error: ${errmsg}`, err]);
-  });
+  return whereClause;
+}
 
-  if (res.rowCount === 0) {
-    throw new Error('No results for count call in getRungroupsList');
-  } else {
-    total = Number(res.rows[0].count);
-  }
+async function getCount(whereClause, client) {
+  const sql = `SELECT count(*) FROM bedrock.run_groups  ${whereClause.whereClause}`;
+  let res;
 
-  sql = `SELECT * FROM bedrock.run_groups ${sql2}`;
+  try {
+    res = await client.query(sql, whereClause.sqlParams);
+  } catch (error) {
+    throw new Error(`PG error getting rungroup count: ${pgErrorCodes[error.code]}`);
+  }
+  return Number(res.rows[0].count);
+}
+
+async function getBase(offset, count, whereClause, client) {
+  let sql = `SELECT * FROM bedrock.run_groups ${whereClause.whereClause}`;
   sql += ' order by run_group_name asc';
   sql += ` offset ${offset} limit ${count} `;
-  console.log('run sql2 = ', sql);
+  let res;
 
-  res = await client.query(sql, sqlParams).catch((err) => {
-    const errmsg = pgErrorCodes[err.code];
-    console.log(err, errmsg);
-    throw new Error([`Postgres error: ${errmsg}`, err]);
-  });
-  await client.end();
-
-  if (res.rowCount === 0) {
-    result.error = true;
-    result.message += 'Rungroup not found';
-  } else {
-    let url = null;
-    if (offset + res.rowCount < total) {
-      const newOffset = parseInt(offset, 10) + res.rowCount;
-      url = `https://${domainName}/${pathElements.join('/')}${qParams}`;
-      url += `${qPrefix}offset=${newOffset.toString()}`;
-    }
-    result.result = {
-      items: res.rows,
-      offset,
-      count: res.rowCount,
-      total,
-      url,
-    };
+  try {
+    res = await client.query(sql, whereClause.sqlParams);
+  } catch (error) {
+    throw new Error(`PG error getting rungroup base information: ${pgErrorCodes[error.code]}`);
   }
+  return res;
+}
+
+function buildURL(domainName, res, offset, total, pathElements) {
+  const qPrefix = '?';
+  const qParams = '';
+  let url = null;
+  if (offset + res.rowCount < total) {
+    const newOffset = parseInt(offset, 10) + res.rowCount;
+    url = `https://${domainName}/${pathElements.join('/')}${qParams}`;
+    url += `${qPrefix}offset=${newOffset.toString()}`;
+  }
+  return url;
+}
+
+async function getRungroupList(domainName, pathElements, queryParams, connection) {
+  const result = {
+    error: false,
+    message: checkParameters(queryParams),
+    result: null,
+  };
+
+  let client;
+  let total;
+  let res;
+  let url;
+  const count = buildCount(queryParams);
+  const offset = buildOffset(queryParams);
+  const whereClause = buildWhereClause(queryParams);
+
+  try {
+    client = await newClient(connection);
+  } catch (error) {
+    result.error = true;
+    result.message = error.message;
+    return result;
+  }
+
+  try {
+    total = await getCount(whereClause, client);
+    if (total === 0) {
+      result.message = 'No rungroups found.';
+      result.result = {
+        items: [],
+        offset,
+        count: 0,
+        total,
+        url,
+      };
+      return result;
+    }
+    res = await getBase(offset, count, whereClause, client);
+    url = buildURL(queryParams, domainName, res, offset, total, pathElements);
+  } catch (error) {
+    await client.end();
+    result.error = true;
+    result.message = error.message;
+    return result;
+  }
+
+  result.result = {
+    items: res.rows,
+    offset,
+    count: res.rowCount,
+    total,
+    url,
+  };
+
   return result;
 }
 
