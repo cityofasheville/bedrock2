4832851e047e36f9ecb2224585f051644c49a981
CameronHenshaw <49311293+CameronHenshaw@users.noreply.github.com>
2023-11-30 11:35:48 -0500

    cleaning two more files (#181)
    * cleaning two more files
    
    * made changes to both files
    
    ---------
    
    Co-authored-by: Cameron Henshaw <cameronhenshaw@Camerons-MBP.lan>

--- src/api/lambdas/bedrock-api-backend/assets/getAllAssetRelations.js
+++ src/api/lambdas/bedrock-api-backend/assets/getAllAssetRelations.js
@@ -2,53 +2,41 @@
 const { Client } = require('pg');
 const pgErrorCodes = require('../pgErrorCodes');
 
-async function getAllAssetRelations(pathElements, queryParams, connection) {
-  let res;
-  const result = {
-    error: false,
-    message: '',
-    result: {},
-  };
-
+async function newClient(connection) {
   const client = new Client(connection);
-  await client.connect()
-    .catch((err) => {
-      result.error = true;
-      result.message = `PG error connecting: ${pgErrorCodes[err.code]}`;
-    });
-
-  if (!result.error) {
-    const sql = `SELECT a.asset_name
-    FROM bedrock.assets a where a.asset_name like $1`;
-
-    res = await client.query(sql, [pathElements[1]])
-      .catch((err) => {
-        result.error = true;
-        result.message = `PG error getting asset information: ${pgErrorCodes[err.code]}`;
-      });
+  try {
+    await client.connect();
+    return client;
+  } catch (error) {
+    throw new Error(`PG error connecting: ${pgErrorCodes[error.code]}`);
   }
+}
 
-  if (result.error) {
-    result.result = null;
-    return result;
+async function readAsset(client, assetName) {
+  let res;
+  const sql = `SELECT a.asset_name 
+  FROM bedrock.assets a where a.asset_name like $1`;
+  try {
+    res = await client.query(sql, [assetName]);
+  } catch (error) {
+    throw new Error(`PG error getting asset information: ${pgErrorCodes[error.code]}`);
   }
+  return res;
+}
 
-  if (res.rowCount === 0) {
-    result.error = true;
-    result.message = 'Asset not found';
-  } else {
-    result.result = {
-      ancestors: {
-        items: [],
-        unique_items: [],
-      },
-      descendants: {
-        items: [],
-        unique_items: [],
-      },
-    };
-
-    let sql = `
+async function readRelations(client, assetName) {
+  let res;
+  const relations = {
+    ancestors: {
+      items: [],
+      unique_items: [],
+    },
+    descendants: {
+      items: [],
+      unique_items: [],
+    },
+  };
+  let sql = `
         WITH RECURSIVE subdependencies AS (
           SELECT asset_name, dependency FROM dependencies
           WHERE asset_name = $1
@@ -58,23 +46,28 @@ async function getAllAssetRelations(pathElements, queryParams, connection) {
             INNER JOIN subdependencies s ON s.dependency = d.asset_name
         ) SELECT * FROM subdependencies;
       `;
-    let check = {};
-    res = await client.query(sql, [pathElements[1]]);
-    for (let i = 0; i < res.rowCount; i += 1) {
-      result.result.ancestors.items.push(
-        {
-          asset_name: res.rows[i].asset_name,
-          dependency: res.rows[i].dependency,
-        },
-      );
-      if (!(res.rows[i].dependency in check)) {
-        result.result.ancestors.unique_items.push(res.rows[i].dependency);
-        check[res.rows[i].dependency] = true;
-      }
-    }
+  let check = {};
+  try {
+    res = await client.query(sql, [assetName]);
+  } catch (error) {
+    throw new Error(`PG error getting ancestor information: ${pgErrorCodes[error.code]}`);
+  }
 
-    // Now the other direction
-    sql = `
+  for (let i = 0; i < res.rowCount; i += 1) {
+    relations.ancestors.items.push(
+      {
+        asset_name: res.rows[i].asset_name,
+        dependency: res.rows[i].dependency,
+      },
+    );
+    if (!(res.rows[i].dependency in check)) {
+      relations.ancestors.unique_items.push(res.rows[i].dependency);
+      check[res.rows[i].dependency] = true;
+    }
+  }
+
+  // Now the other direction
+  sql = `
         WITH RECURSIVE subdependencies AS (
           SELECT asset_name, dependency FROM dependencies
           WHERE dependency = $1
@@ -84,21 +77,77 @@ async function getAllAssetRelations(pathElements, queryParams, connection) {
             INNER JOIN subdependencies s ON s.asset_name = d.dependency
         ) SELECT * FROM subdependencies;
       `;
-    check = {};
-    res = await client.query(sql, [pathElements[1]]);
-    for (let i = 0; i < res.rowCount; i += 1) {
-      result.result.descendants.items.push(
-        {
-          asset_name: res.rows[i].asset_name,
-          dependency: res.rows[i].dependency,
-        },
-      );
-      if (!(res.rows[i].asset_name in check)) {
-        result.result.descendants.unique_items.push(res.rows[i].asset_name);
-        check[res.rows[i].asset_name] = true;
-      }
+  check = {};
+  try {
+    res = await client.query(sql, [assetName]);
+  } catch (error) {
+    throw new Error(`PG error getting descendent information: ${pgErrorCodes[error.code]}`);
+  }
+  for (let i = 0; i < res.rowCount; i += 1) {
+    relations.descendants.items.push(
+      {
+        asset_name: res.rows[i].asset_name,
+        dependency: res.rows[i].dependency,
+      },
+    );
+    if (!(res.rows[i].asset_name in check)) {
+      relations.descendants.unique_items.push(res.rows[i].asset_name);
+      check[res.rows[i].asset_name] = true;
     }
   }
+  return relations;
+}
+
+async function getAllAssetRelations(pathElements, queryParams, connection) {
+  const assetName = pathElements[1];
+  let client;
+  let relations;
+  let res;
+  const result = {
+    error: false,
+    message: '',
+    result: {
+      ancestors: {
+        items: [],
+        unique_items: [],
+      },
+      descendants: {
+        items: [],
+        unique_items: [],
+      },
+    },
+  };
+
+  try {
+    client = await newClient(connection);
+  } catch (error) {
+    result.error = true;
+    result.message = error.message;
+    return result;
+  }
+
+  try {
+    res = await readAsset(client, assetName);
+    if (res.rowCount === 0) {
+      result.message = 'No assets found';
+      return result;
+    }
+    relations = await readRelations(client, assetName);
+  } catch (error) {
+    await client.end();
+    result.error = true;
+    result.message = error.message;
+    await client.end();
+    return result;
+  }
+
+  await client.end();
+
+  result.result.ancestors.items = relations.ancestors.items;
+  result.result.ancestors.unique_items = relations.ancestors.unique_items;
+  result.result.descendants.items = relations.descendants.items;
+  result.result.descendants.unique_items = relations.descendants.unique_items;
+
   return result;
 }
 
