b241842aea0d07b6e9632d689a8a66a9892e35ac
Cameron Henshaw <cameronhenshaw@it-mba-ch.lan>
2024-05-30 13:16:36 -0400

    GET rungroup now returns array of assets in rungroup

--- src/api/lambdas/bedrock-api-backend/run_groups/getRunGroup.js
+++ src/api/lambdas/bedrock-api-backend/run_groups/getRunGroup.js
@@ -2,6 +2,23 @@
 /* eslint-disable no-console */
 import { newClient, getInfo } from '../utilities/utilities.js';
 
+async function getAssetInfo(client, idValue, name) {
+  // Querying database to get information. Function can be used multiple times per method
+  // if we need information from multiple tables
+  const sql = `SELECT * FROM bedrock.etl where run_group like $1`;
+  let res;
+  try {
+    res = await client.query(sql, [idValue]);
+  } catch (error) {
+    throw new Error([`Postgres error: ${pgErrorCodes[error.code]}`, error]);
+  }
+
+  if (res.rowCount === 0) {
+    throw new Error(`Asse information for ${name} not found`);
+  }
+  return res;
+}
+
 async function getRunGroup(
   connection,
   idField,
@@ -22,6 +39,8 @@ async function getRunGroup(
     client = await newClient(connection);
     clientInitiated = true;
     response.result = await getInfo(client, idField, idValue, name, tableName);
+    const assetInfo = await getAssetInfo(client, idValue, name);
+    response.result.assets = assetInfo.rows.map((obj) => obj.asset_name);
     await client.end();
   } catch (error) {
     if (clientInitiated) {
