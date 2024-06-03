c682647c766bb080b4b9e364f199a028653bc39e
Cameron Henshaw <cameronhenshaw@it-mba-ch.lan>
2024-06-03 11:54:28 -0400

    get rungroup now returns both asset name and asset display name

--- src/api/lambdas/bedrock-api-backend/run_groups/getRunGroup.js
+++ src/api/lambdas/bedrock-api-backend/run_groups/getRunGroup.js
@@ -1,11 +1,12 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 import { newClient, getInfo } from '../utilities/utilities.js';
+import pgErrorCodes from '../pgErrorCodes.js';
 
 async function getAssetInfo(client, idValue, name) {
   // Querying database to get information. Function can be used multiple times per method
   // if we need information from multiple tables
-  const sql = `SELECT * FROM bedrock.etl where run_group like $1`;
+  const sql = 'SELECT a.*, b.display_name FROM bedrock.etl a left join bedrock.assets b on a.asset_name = b.asset_name where run_group like $1';
   let res;
   try {
     res = await client.query(sql, [idValue]);
@@ -14,7 +15,7 @@ async function getAssetInfo(client, idValue, name) {
   }
 
   if (res.rowCount === 0) {
-    throw new Error(`Asse information for ${name} not found`);
+    throw new Error(`Asset information for ${name} not found`);
   }
   return res;
 }
@@ -40,7 +41,11 @@ async function getRunGroup(
     clientInitiated = true;
     response.result = await getInfo(client, idField, idValue, name, tableName);
     const assetInfo = await getAssetInfo(client, idValue, name);
-    response.result.assets = assetInfo.rows.map((obj) => obj.asset_name);
+    console.log(assetInfo);
+    response.result.assets = [];
+    assetInfo.rows.forEach((obj) => {
+      response.result.assets.push({ asset_name: obj.asset_name, display_name: obj.display_name }); 
+    });
     await client.end();
   } catch (error) {
     if (clientInitiated) {
