cbb5f7e086890755514396a594c38bad75715cdc
Jon Wilson <jontwilson@gmail.com>
2024-06-11 11:32:19 -0400

    state machine param one_asset (#243)
    Co-authored-by: Jon Wilson <jtwilson@ashevillenc.gov>

--- src/etl/lambdas/create_etl_run_map/local.js
+++ src/etl/lambdas/create_etl_run_map/local.js
@@ -1,5 +1,6 @@
 import { lambda_handler } from './handler.js';
-let event = { run_group: 'daily', debug: true };
+// let event = { run_group: 'vxsmart_balances', debug: true };
+let event = { one_asset: 'permit_tasks.lib', debug: true };
 
 await lambda_handler(event);
 
