37cae5c52b4ff69c69ff4118ca5429dad4c3afae
Jon Wilson <jtwilson@ashevillenc.gov>
2024-07-02 10:13:32 -0400

    all views include ids

--- docs/NewDBExperiment/CreateNewViews.sql
+++ docs/NewDBExperiment/CreateNewViews.sql
@@ -1,5 +1,6 @@
 create view bedrock.asset_tag_view as
-select a.asset_name, tags.tag_name 
+select at2.asset_id, a.asset_name, 
+at2.tag_id, tags.tag_name 
 from bedrock.asset_tags at2
 inner join bedrock.assets a
 on at2.asset_id = a.asset_id 
@@ -7,7 +8,8 @@ inner join bedrock.tags
 on at2.tag_id = tags.tag_id;
 
 create view bedrock.asset_type_custom_field_view as
-select asset_type_name, custom_field_name, required
+select atcf.asset_type_id,  asset_type_name, 
+atcf.custom_field_id, custom_field_name, required
 from bedrock.asset_types at2 
 inner join bedrock.asset_type_custom_fields atcf
 on at2.asset_type_id = atcf.asset_type_id
@@ -15,13 +17,14 @@ inner join bedrock.custom_fields cf
 on atcf.custom_field_id = cf.custom_field_id; 
 
 create view bedrock.asset_type_view as
-select at2.asset_type_name, at3.asset_type_name parent_name
+select at2.asset_type_id, at2.asset_type_name, 
+at2.parent, at3.asset_type_name parent_name
 from bedrock.asset_types at2 
 left join bedrock.asset_types at3 
 on at2.parent = at3.asset_type_id;
 
 create view bedrock.asset_view as
-select asset_id, asset_name, description, "location", asset_type_name, owner_id, notes, link, active
+select asset_id, asset_name, description, "location", a.asset_type_id, asset_type_name, owner_id, notes, link, active
 FROM bedrock.assets a
 left join bedrock.asset_types at2
 on a.asset_type_id = at2.asset_type_id;
@@ -55,7 +58,7 @@ on a2.asset_name = t.source->>'asset'
 where t.type in ('table_copy','file_copy');
 
 create view bedrock.etl_view as 
-select asset_name, run_group_name, etl.active 
+select etl.asset_id, asset_name, etl.run_group_id, run_group_name, etl.active 
 from bedrock.etl
 inner join bedrock.assets
 on etl.asset_id = assets.asset_id 
@@ -63,13 +66,14 @@ inner join bedrock.run_groups rg
 on etl.run_group_id = rg.run_group_id; 
 
 create view bedrock.task_view as
-SELECT asset_name, seq_number, tasks.description, "type", tasks.active, "source", target, "configuration"
+SELECT tasks.task_id, tasks.asset_id, asset_name, seq_number, tasks.description, "type", tasks.active, "source", target, "configuration"
 FROM bedrock.tasks
 inner join bedrock.assets a 
 on tasks.asset_id = a.asset_id; 
 
 create view bedrock.custom_value_view as
-select a.asset_name, cf.custom_field_name, cf.field_type, cf.field_data 
+select cv.asset_id, a.asset_name,
+cv.custom_field_id, cf.custom_field_name, cf.field_type, cf.field_data 
 from bedrock.custom_values cv
 inner join bedrock.assets a
 on cv.asset_id = a.asset_id 
