bf84b5763972d20d949e80a0c116575e2ed0c825
Eric Jackson <ejackson@ashevillenc.gov>
2024-07-03 16:24:49 -0400

    Add general version of local script and JSON file with existing local.py test content

---
+++ src/etl/lambdas/etl_task_file_copy/etl_filecopy.py
@@ -0,0 +1,16 @@
+from handler import lambda_handler
+import sys
+import json
+
+event = {}
+context = {}
+
+filename = 'localtest.json'
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+
+f = open(filename)
+event = json.load(f)
+f.close()
+
+lambda_handler(event, context)
