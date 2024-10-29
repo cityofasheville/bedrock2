319a42c42aa4f2e6f693966b1ca256caeed25dc7
CameronHenshaw <49311293+CameronHenshaw@users.noreply.github.com>
2024-10-29 11:08:45 -0800

    Better fix, which prioritizes messages

--- src/api/lambdas/bedrock-api-backend/handler.js
+++ src/api/lambdas/bedrock-api-backend/handler.js
@@ -144,10 +144,10 @@ export async function lambda_handler(event) {
       break;
   }
   let bodyJSON = {};
-  if (api_result.result) {
-    bodyJSON = api_result.result;
-  } else if (api_result.message) {
+  if (api_result.message) {
     bodyJSON.message = api_result.message.message || api_result.message;
+  } else if (api_result.result) {
+    bodyJSON = api_result.result;
   }
   let retvalue = {
     statusCode: api_result.statusCode,
