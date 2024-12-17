7427f7e79fc91cc6a866b94bd9357d0bbb2d0e4e
Jon Wilson <jtwilson@ashevillenc.gov>
2024-12-17 12:20:35 -0500

    cleanup

--- src/db/bedrock-db/deploy/config.tf
+++ src/db/bedrock-db/deploy/config.tf
@@ -14,14 +14,14 @@ resource "aws_db_instance" "bedrock-db-$$INSTANCE$$" {
   parameter_group_name = "default.postgres13"
   skip_final_snapshot  = true
   publicly_accessible  = true
-  db_subnet_group_name = $$DB_SUBNET_GROUP_NAME$$
+  db_subnet_group_name = "$$DB_SUBNET_GROUP_NAME$$" 
   vpc_security_group_ids = [aws_security_group.bedrock-pg-sg-$$INSTANCE$$.id]
 }
 
 resource "aws_security_group" "bedrock-pg-sg-$$INSTANCE$$" {
   name        = "bedrock-pg-sg-$$INSTANCE$$"
   description = "Allow public access to postgres"
-  vpc_id      = $$BEDROCK_VPC_ID$$
+  vpc_id      = "$$BEDROCK_VPC_ID$$"
 
   ingress {
     description      = "Inbound access to postgres"
